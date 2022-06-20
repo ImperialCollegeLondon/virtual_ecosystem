17c20c38d30ec848a54b813203006e5c142f5d2a
dorme <d.orme@imperial.ac.uk>
2022-06-20 09:49:10 +0100

    Training files for pytest/doctest; also init of git flow branches

---
+++ source/development/training/mfm.py
@@ -0,0 +1,114 @@
+"""Demonstrate docstring.
+
+Some example code used to demonstrate docstrings
+"""
+
+# flake8: noqa D202, D107
+
+from typing import List
+
+
+def my_float_multiplier(x: float, y: float) -> float:
+    """Multiplies two floats together.
+
+    Arguments:
+        x: The first number
+        y: The second number
+
+    Examples:
+        >>> my_float_multiplier(2.1, 3.6)
+        7.56
+    """
+
+    return x * y
+
+def my_float_multiplier2(x: float, y: float) -> float:
+    """Multiplies two floats together.
+
+    Arguments:
+        x: The first number
+        y: The second number
+
+    Examples:
+        >>> round(my_float_multiplier2(2.1, 3.6), 2)
+        7.56
+    """
+    
+    return x * y
+
+    
+
+def my_float_multiplier3(x: float, y: float) -> float:
+    """Multiplies two floats together.
+
+    Arguments:
+        x: The first number
+        y: The second number
+
+    Examples:
+        >>> my_float_multiplier3(2.1, 3.6) #doctest: +ELLIPSIS
+        7.56...
+    """
+
+    return x * y
+
+
+def my_picky_float_multiplier(x: float, y: float) -> float:
+    """Multiplies two floats together.
+
+    Arguments:
+        x: The first number
+        y: The second number
+
+    Examples:
+        >>> my_float_multiplier3(2.1, 3.6) #doctest: +ELLIPSIS
+        7.56...
+    """
+
+    if not (isinstance(x, float) and isinstance(y, float)):
+        raise ValueError('Both x and y must be of type float')
+    
+    return x * y
+
+
+class TimesTable:
+    """Create times tables for a number.
+
+    The TimesTable instance can be used to produce a times
+    table for a specific number.
+
+    Attributes:
+        num: The base number to use for tables
+
+    Args:
+        num: Sets the `num` attribute
+
+    Examples:
+        >>> seven_tt = TimesTable(num = 7)
+        >>> seven_tt  # doctest: +ELLIPSIS
+        <mfm.TimesTable object at 0x...>
+        >>> seven_tt.num
+        7
+    """
+
+    def __init__(self, num: int) -> None:
+
+        self.num = num
+
+    def table(self, start: int = 1, stop: int = 10) -> List[int]:
+        """Calculate a table.
+
+        Returns a times table for the initialised base number from
+        start to stop.
+
+        Args:
+            start: Start number for the table
+            stop: End number for the table
+
+        Examples:
+            >>> seven_tt = TimesTable(num = 7)
+            >>> seven_tt.table(2,7)
+            [14, 21, 28, 35, 42, 49]
+        """
+
+        return [self.num * v for v in range(start, stop + 1)]
---
+++ source/development/training/test_mfm.py
@@ -0,0 +1,92 @@
+# flake8: noqa D103 - docstrings on unit tests
+
+import pytest
+
+from mfm import my_float_multiplier, my_picky_float_multiplier, TimesTable
+
+
+def test_fm():
+
+    assert 10 == my_float_multiplier(2, 5)
+
+
+def test_pfm():
+
+    with pytest.raises(ValueError) as err_hndlr:
+
+        x = my_picky_float_multiplier(2, 5)
+
+    assert str(err_hndlr.value) == "Both x and y must be of type float"
+
+
+def test_pfm_fail():
+
+    with pytest.raises(ValueError) as err_hndlr:
+
+        x = my_picky_float_multiplier(2.0, 5.0)
+
+    assert str(err_hndlr.value) == "Both x and y must be of type float"
+
+
+
+@pytest.mark.parametrize(
+    argnames=["x", "y", "expected"],
+    argvalues=[
+        (1.0, 3.0, 3.0),
+        (1.5, -3.0, -4.5),
+        (-1.5, 3.0, -4.5),
+        (-1.5, -3.0, 4.5),
+    ],
+)
+def test_pfm_param_noid(x, y, expected):
+
+    assert expected == my_picky_float_multiplier(x, y)
+
+
+@pytest.mark.parametrize(
+    argnames=["x", "y", "expected"],
+    argvalues=[
+        (1.5, 3.0, 4.5),
+        (1.5, -3.0, -4.5),
+        (-1.5, 3.0, -4.5),
+        (-1.5, -3.0, 4.5),
+    ],
+    ids=["++", "-+", "+-", "--"],
+)
+def test_pfm_param_ids(x, y, expected):
+
+    assert expected == my_picky_float_multiplier(x, y)
+
+
+@pytest.mark.parametrize(
+    argnames=["x"],
+    argvalues=[(1.5,), (-1.5,)],
+    ids=["+", "-"],
+)
+@pytest.mark.parametrize(
+    argnames=["y"],
+    argvalues=[(3.0,), (-3.0,)],
+    ids=["+", "-"],
+)
+def test_pfm_2param_ids(x, y):
+
+    assert 4.5 == abs(my_picky_float_multiplier(x, y))
+
+
+@pytest.fixture()
+def times_table_instance():
+    return TimesTable(num=7)
+
+
+def test_times_table_errors(times_table_instance):
+
+    with pytest.raises(TypeError) as err_hndlr:
+        times_table_instance.table(1.6, 23.9)
+
+    assert str(err_hndlr.value) == "'float' object cannot be interpreted as an integer"
+
+
+def test_times_table_values(times_table_instance):
+
+    value = times_table_instance.table(2, 7)
+    assert value == [14, 21, 28, 35, 42, 49]
