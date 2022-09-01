1c2ccab6ce997e374e55c6827c499f86b047427b
Jacob Cook <jc2017@ic.ac.uk>
2022-09-01 10:20:02 +0100

    Wrote a test schema for plants module

--- virtual_rainforest/plants/__init__.py
+++ virtual_rainforest/plants/__init__.py
@@ -5,6 +5,31 @@ from virtual_rainforest.core.config import register_schema
 def core_schema():
     """Defines the schema that the plant module configuration should conform to."""
 
-    config_schema = {}
+    config_schema = {
+        "type": "object",
+        "properties": {
+            "config": {
+                "type": "object",
+                "properties": {
+                    "plants": {
+                        "description": "Configuration settings for the plants module",
+                        "type": "object",
+                        "properties": {
+                            "ftypes": {
+                                "description": "Details of the plant functional types",
+                                "type": "array",
+                            }
+                        },
+                        "required": ["ftypes"],
+                        "additionalProperties": False,
+                    }
+                },
+                "required": ["plants"],
+                "additionalProperties": False,
+            }
+        },
+        "required": ["config"],
+        "additionalProperties": False,
+    }
 
     return config_schema
