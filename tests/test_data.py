7c157d89921cbaea22b7458dc75830dc69e43683
dorme <d.orme@imperial.ac.uk>
2022-09-26 17:11:01 +0100

    First draft of test_data and input files

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,22 +1,29 @@
 """Test data loading and validation."""
 
+import os
 from contextlib import contextmanager
+from typing import Generator
 
 import pytest
-
-# TODO - look at sharing does_not_raise() - and probably other helpers - across test
-#        files: see https://stackoverflow.com/questions/33508060/
+from xarray import load_dataset
 
 
 @contextmanager
-def does_not_raise():
+def does_not_raise() -> Generator:
     """Handle inputs that do not raise exceptions in context managers.
 
-    The common structure of `with pytest.raises() as excep:` is really useful for
-    setting up paramaterised tests of failure modes. This function behaves as a similar
-    context manager for inputs that do _not_ raise an exception, so that passing and
-    failing inputs can be used in the same test.
+    The pytest structure of `with pytest.raises(ErrorType) as excep:` is a standard way
+    of testing for expected errors. With parameterised tests, the expected error (e.g.
+    `pytest.raises(ValueError)`) can be passed in as a parameter. _This_ function
+    provides a parameter value that behaves as a similar context manager for inputs
+    that do _not_ raise an exception, so that passing and failing inputs can be used in
+    the same test.
+
+    Note that the output of str(does_not_raise) is 'None'.
     """
+
+    # TODO - look at sharing does_not_raise() - and probably other helpers - across test
+    #        files: see https://stackoverflow.com/questions/33508060/
 
     yield
 
@@ -28,6 +35,7 @@ def fixture_square_grid():
     A 10 x 10 grid of 1 hectare cells, with non-zero origin.
     """
 
+    # TODO - can't type the return  value without a top level import of Grid
     from virtual_rainforest.core.grid import Grid
 
     grid = Grid(
@@ -85,3 +93,75 @@ def test_check_coordinates_in_grid(
         check_coordinates_in_grid(fixture_square_grid, x_coord, y_coord)
 
         assert str(excep) == exp_message
+
+
+@pytest.mark.parametrize(
+    argnames=["filename", "expected_outcome", "expected_outcome_msg"],
+    argvalues=[
+        ("two_dim_xy.nc", does_not_raise(), "None"),
+        (
+            "two_dim_xy_6by10.nc",
+            pytest.raises(ValueError),
+            "Data xy dimensions do not match grid",
+        ),
+        (
+            "two_dim_xy_lowx.nc",
+            pytest.raises(ValueError),
+            "Data coordinates do not align with grid coordinates.",
+        ),
+        ("two_dim_idx.nc", does_not_raise(), "None"),
+        (
+            "two_dim_idx_6by10.nc",
+            pytest.raises(ValueError),
+            "Data xy dimensions do not match grid",
+        ),
+        ("one_dim_cellid.nc", does_not_raise(), "None"),
+        (
+            "one_dim_cellid_lown.nc",
+            pytest.raises(ValueError),
+            "Grid defines 100 cells, data provides 60",
+        ),
+        ("one_dim_points_xy.nc", does_not_raise(), "None"),
+        (
+            "one_dim_points_xy_xney.nc",
+            pytest.raises(ValueError),
+            "The cell_ids in the data do not match grid cell ids.",
+        ),
+        (
+            "one_dim_cellid_badid.nc",
+            pytest.raises(ValueError),
+            "The x and y data have different dimensions",
+        ),
+        ("one_dim_points_order_only.nc", does_not_raise(), "None"),
+    ],
+    ids=[
+        "two_dim_xy",
+        "two_dim_xy_6by10",
+        "two_dim_xy_lowx",
+        "two_dim_idx",
+        "two_dim_idx_6by10",
+        "one_dim_cellid",
+        "one_dim_cellid_lown",
+        "one_dim_cellid_badid",
+        "one_dim_points_xy",
+        "one_dim_points_xy_xney",
+        "one_dim_points_order_only",
+    ],
+)
+def test_map_dataset_onto_square_grid(
+    fixture_square_grid, datadir, filename, expected_outcome, expected_outcome_msg
+):
+    """Test ability to map NetCDF files.
+
+    The test parameters include both passing and failing files, stored in test_data.
+    """
+    from virtual_rainforest.core.data import map_dataset_onto_square_grid
+
+    datafile = os.path.join(datadir, filename)
+    dataset = load_dataset(datafile)
+
+    with expected_outcome as outcome:
+
+        map_dataset_onto_square_grid(fixture_square_grid, dataset)
+
+        assert str(outcome) == expected_outcome_msg
---
+++ tests/test_data/example_netcdf_generator.py
@@ -0,0 +1,212 @@
+"""Generate NetCDF datasets for testing."""
+
+from itertools import product
+
+import numpy as np
+import xarray
+
+# https://towardsdatascience.com/basic-data-structures-of-xarray-80bab8094efa
+
+x_n = 10
+y_n = 10
+res = 100
+x_off = 500000
+y_off = 200000
+
+# Row and column indices
+x_idx = np.arange(x_n)
+y_idx = np.arange(y_n)
+
+# Cell centre coordinates
+x = x_off + x_idx * res + res / 2
+y = y_off + y_idx * res + res / 2
+
+xx, yy = np.meshgrid(x, y)
+
+# Cell IDs
+cell_id = np.array([f"{x}-{y}" for x, y in product(x_idx, y_idx)])
+
+# Explicit row numbers
+row = np.arange(x_n * y_n)
+
+# Some data
+rng = np.random.default_rng()
+
+grid_size = (x_n, y_n)
+
+temp = rng.normal(loc=20, scale=3, size=grid_size)
+prec = rng.lognormal(mean=5, sigma=1, size=grid_size)
+
+# ----------------------------------------------------------------------
+# Generate NetCDF with 2D grid with actual coordinates on dimensions
+# ----------------------------------------------------------------------
+
+# - good version.
+ds_2xy = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp, coords=[x, y], dims=["x", "y"]),
+        "prec": xarray.DataArray(prec, coords=[x, y], dims=["x", "y"]),
+    }
+)
+
+ds_2xy.to_netcdf("two_dim_xy.nc")
+
+# - bad: extent not x_n by y_n
+sset = np.arange(6)
+
+ds_2xy_6by10 = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp[sset], coords=[x[sset], y], dims=["x", "y"]),
+        "prec": xarray.DataArray(prec[sset], coords=[x[sset], y], dims=["x", "y"]),
+    }
+)
+
+ds_2xy_6by10.to_netcdf("two_dim_xy_6by10.nc")
+
+
+# - bad: coordinates don't match
+
+ds_2xy_lowx = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp, coords=[x - 3e6, y], dims=["x", "y"]),
+        "prec": xarray.DataArray(prec, coords=[x - 3e6, y], dims=["x", "y"]),
+    }
+)
+
+ds_2xy_lowx.to_netcdf("two_dim_xy_lowx.nc")
+
+# ----------------------------------------------------------------------
+# Generate NetCDF with 2D grid with no coordinates on dimension
+# ----------------------------------------------------------------------
+
+# - Good version
+ds_2idx = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp, dims=["x", "y"]),
+        "prec": xarray.DataArray(prec, dims=["x", "y"]),
+    }
+)
+
+ds_2idx.to_netcdf("two_dim_idx.nc")
+
+# - bad: extent not x_n by y_n
+sset = np.arange(6)
+
+ds_2idx_6by10 = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp[sset], coords=[x[sset], y], dims=["x", "y"]),
+        "prec": xarray.DataArray(prec[sset], coords=[x[sset], y], dims=["x", "y"]),
+    }
+)
+
+ds_2idx_6by10.to_netcdf("two_dim_idx_6by10.nc")
+
+
+# ----------------------------------------------------------------------
+# Generate NetCDF with column data indexed by cell id - if we use an integer cell id
+# then this collapses to the example dataframe with assumed order?
+# ----------------------------------------------------------------------
+
+# Good version
+
+ds_1cid = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp.flatten(), coords=[cell_id], dims=["cell_id"]),
+        "prec": xarray.DataArray(prec.flatten(), coords=[cell_id], dims=["cell_id"]),
+    }
+)
+
+ds_1cid.to_netcdf("one_dim_cellid.nc")
+
+# Bad version - doesn't cover cells
+lown = np.arange(60)
+
+ds_1cid_lown = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(
+            temp.flatten()[lown], coords=[cell_id[lown]], dims=["cell_id"]
+        ),
+        "prec": xarray.DataArray(
+            prec.flatten()[lown], coords=[cell_id[lown]], dims=["cell_id"]
+        ),
+    }
+)
+
+ds_1cid_lown.to_netcdf("one_dim_cellid_lown.nc")
+
+# Bad version - IDs don't match
+cell_id_no = np.char.add(cell_id, ["no!"])
+
+ds_1cid_badid = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(temp.flatten(), coords=[cell_id_no], dims=["cell_id"]),
+        "prec": xarray.DataArray(prec.flatten(), coords=[cell_id_no], dims=["cell_id"]),
+    }
+)
+
+ds_1cid_badid.to_netcdf("one_dim_cellid_badid.nc")
+
+# ----------------------------------------------------------------------
+# Generate NetCDF with column data with a row dimension integer, including x and y
+# data (A data frame, basically)
+# ----------------------------------------------------------------------
+
+# Good version
+ds_df = xarray.Dataset(
+    data_vars={
+        "temperature": xarray.DataArray(temp.flatten(), dims=["row"]),
+        "precipitation": xarray.DataArray(prec.flatten(), dims=["row"]),
+        "x": xarray.DataArray(xx.flatten(), dims=["row"]),
+        "y": xarray.DataArray(yy.flatten(), dims=["row"]),
+    }
+)
+
+ds_df.to_netcdf("one_dim_points_xy.nc")
+
+# Bad version - x and y don't align
+
+ds_df_xney = xarray.Dataset(
+    data_vars={
+        "temperature": xarray.DataArray(temp.flatten(), dims=["row"]),
+        "precipitation": xarray.DataArray(prec.flatten(), dims=["row"]),
+        "x": xarray.DataArray(xx.flatten()[0:60], dims=["row_short"]),
+        "y": xarray.DataArray(yy.flatten(), dims=["row"]),
+    }
+)
+
+ds_df_xney.to_netcdf("one_dim_points_xy_xney.nc")
+
+
+# Bad version - doesn't cover cells
+
+lown = np.arange(60)
+
+ds_1cid_lown = xarray.Dataset(
+    data_vars={
+        "temp": xarray.DataArray(
+            temp.flatten()[lown], coords=[cell_id[lown]], dims=["row"]
+        ),
+        "prec": xarray.DataArray(
+            prec.flatten()[lown], coords=[cell_id[lown]], dims=["row"]
+        ),
+        "x": xarray.DataArray(xx.flatten()[lown], dims=["row"]),
+        "y": xarray.DataArray(yy.flatten()[lown], dims=["row"]),
+    }
+)
+
+ds_df_xney.to_netcdf("one_dim_points_xy_lown.nc")
+
+
+# ----------------------------------------------------------------------
+# Generate NetCDF with column data with a row_idx dimension integer, _not_ including x
+# and y data (A data frame, basically, but with an assumed order).
+# ----------------------------------------------------------------------
+
+ds_df = xarray.Dataset(
+    data_vars={
+        "temperature": xarray.DataArray(temp.flatten(), dims=["cell_id"]),
+        "precipitation": xarray.DataArray(prec.flatten(), dims=["cell_id"]),
+    }
+)
+
+ds_df.to_netcdf("one_dim_points_order_only.nc")
