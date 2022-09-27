84bc5d7c475ce1ffa5218f01f7581afeabf4e666
Jacob Cook <jc2017@ic.ac.uk>
2022-09-27 11:17:11 +0100

    Moved schema population imports to highest level init

--- virtual_rainforest/__init__.py
+++ virtual_rainforest/__init__.py
@@ -1,5 +1,9 @@
 import logging
 
+# Import all module schema here to ensure that they are added to the registry
+from virtual_rainforest.core import schema  # noqa
+from virtual_rainforest.plants import schema  # noqa
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] - %(module)s - %(funcName)s(%(lineno)d) - %(message)s",
--- virtual_rainforest/core/__init__.py
+++ virtual_rainforest/core/__init__.py
@@ -3,21 +3,6 @@ import json
 from virtual_rainforest.core.config import register_schema
 
 
-# This is definitely a bit questionable, its basically here to make sure that the plants
-# module is imported somewhere when tests are run, this ensures that the plant schema is
-# added to testing. I don't think this is the best or right way to do something like
-# this, but couldn't come up with a better way to do it
-# TODO - Think of a better solution for this
-def load_plants_schema():
-    """Imports the plants module schema."""
-    from virtual_rainforest.plants import schema
-
-    return schema
-
-
-load_plants_schema()
-
-
 @register_schema("core")
 def schema():
     """Defines the schema that the core module configuration should conform to."""
