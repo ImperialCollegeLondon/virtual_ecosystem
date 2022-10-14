d52850769a738cfdf17951b520ed73cc1c467246
dorme <d.orme@imperial.ac.uk>
2022-10-14 16:31:05 +0100

    Fixing __version__ definition to follow poetry, moved some logger code

--- virtual_rainforest/__init__.py
+++ virtual_rainforest/__init__.py
@@ -1,12 +1,7 @@
-import logging
+import importlib.metadata
 
 # Import all module schema here to ensure that they are added to the registry
 from virtual_rainforest.core import schema  # noqa
 from virtual_rainforest.plants import schema  # noqa
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s] - %(module)s - %(funcName)s(%(lineno)d) - %(message)s",
-)
-
-loggers = logging.getLogger(__name__)
+__version__ = importlib.metadata.version(__name__)
