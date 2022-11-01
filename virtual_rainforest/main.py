cf8b23034547c414e7b19e5be9bae253a065dde1
Jacob Cook <jc2017@ic.ac.uk>
2022-11-01 11:53:13 +0000

    Wrote actual docstrings for the main function and script

--- virtual_rainforest/main.py
+++ virtual_rainforest/main.py
@@ -1,4 +1,8 @@
-"""TODO - WRITE DOC STRING DESCRIBING MAIN FUNCTION."""
+"""Defines the function used to run a full simulation of the model.
+
+As well as setting up the function to run the overall virtual rainforest simulation,
+this script also defines the command line entry points for the model.
+"""
 
 from typing import Union
 
@@ -9,10 +13,23 @@ from virtual_rainforest.core.logger import LOGGER
 def vr_run(
     cfg_paths: Union[str, list[str]], output_folder: str, out_file_name: str
 ) -> None:
-    """TODO - DOCSSTRING FOR FUNCTION."""
+    """Perform a virtual rainforest simulation.
+
+    This is a high-level function that runs a virtual rainforest simulation. At the
+    moment this is fairly limited, and just involves validating an input configuration.
+    Down the line this should be extended to encompass far more steps.
+
+    Args:
+        cfg_paths: Set of paths to configuration files
+        output_folder: Folder to save combined configuration to
+        out_file_name: Name for the combined configuration file
+    """
 
     config = validate_config(cfg_paths, output_folder, out_file_name)
 
     print(config)
 
     LOGGER.info("Virtual rainforest model run completed!")
+
+
+# def _vr_run_cli
