a14bfb5025ffc3e3cea90198a80eb282baec42c9
Jacob Cook <jc2017@ic.ac.uk>
2022-11-08 10:05:42 +0000

    Added check for missing models in registry

--- virtual_rainforest/main.py
+++ virtual_rainforest/main.py
@@ -5,7 +5,7 @@ this script also defines the command line entry points for the model.
 """
 
 from copy import deepcopy
-from typing import Union
+from typing import Any, Optional, Union
 
 from virtual_rainforest.core.config import validate_config
 from virtual_rainforest.core.logger import LOGGER
@@ -13,19 +13,31 @@ from virtual_rainforest.core.model import MODEL_REGISTRY
 
 
 # TODO - WORK OUT WHAT THIS FUNCTION SHOULD ACTUALLY RETURN
-def select_models(model_list: list[str]) -> None:
+# SHOULD BE A LIST But question is what type the list should contain
+# TODO - ADD TESTS FOR THIS FUNCTION
+def select_models(config: dict[str, Any]) -> Optional[list]:
     """TODO - WRITE A SENSIBLE DOCSTRING!"""
+
+    model_list = deepcopy(config["core"]["modules"])
 
     # Remove "core" from model list as it is not a model
     if "core" in model_list:
         model_list.remove("core")
 
+    # Make list of missing models, and return an error if necessary
+    miss_model = [model for model in model_list if model not in MODEL_REGISTRY.keys()]
+    if miss_model != []:
+        LOGGER.error(
+            f"The following models cannot be configured as they are not found in the "
+            f"registry: {miss_model}"
+        )
+    return None
+
     # Then look for each model in the registry
     for model in model_list:
+        # CAN ALL OF THESE BE DONE IN ONE STEP???
+        # OR DOES FACTORY HAVE TO BE DONE ONE BY ONE??
         print(MODEL_REGISTRY[model])
-
-    # SOME KIND OF ERROR FOR MODEL NOT FOUND
-    # IDEALLY CATCH THEM ALL IN ONE
 
 
 # TODO - Add tests for this function
@@ -35,8 +47,9 @@ def vr_run(
     """Perform a virtual rainforest simulation.
 
     This is a high-level function that runs a virtual rainforest simulation. At the
-    moment this is fairly limited, and just involves validating an input configuration.
-    Down the line this should be extended to encompass far more steps.
+    moment this involves validating an input configuration, and using this configuration
+    to generate a set of configured model objects suitable for downstream use. Down the
+    line this should be extended to encompass far more steps.
 
     Args:
         cfg_paths: Set of paths to configuration files
@@ -46,10 +59,12 @@ def vr_run(
 
     config = validate_config(cfg_paths, output_folder, out_file_name)
 
-    # TODO - Add in additional model details
+    # TODO - Extract input data required to initialise the models
 
     # TODO -  SELECT MODELS TO BE RUN
-    select_models(deepcopy(config["core"]["modules"]))
+    select_models(config)
+
+    # TODO - Initialise the set of configured models
 
     # TODO - Save model state
 
@@ -59,7 +74,6 @@ def vr_run(
     # TODO - Save model state
 
     LOGGER.info("Virtual rainforest model run completed!")
-    print(config)
 
 
 # TODO - Define command line entry point
