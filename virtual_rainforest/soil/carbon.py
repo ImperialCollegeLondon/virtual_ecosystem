53b361da22d94bb7cda0d5fafb56e850ca004c05
Jacob Cook <jc2017@ic.ac.uk>
2022-12-13 09:32:30 +0000

    Added skelton SoilCarbon class

---
+++ virtual_rainforest/soil/carbon.py
@@ -0,0 +1,27 @@
+"""The `soil.carbon` module.
+
+This module simulates the radiation soil carbon cycle for the Virtual Rainforest.
+TODO - EXPAND THE DETAILS HERE
+
+TODO - EXPLAIN WHAT ISN'T INCLUDED HERE
+"""
+
+
+# OKAY FIRST QUESTION IS WHAT EQUATION SET TO USE
+# WHAT'S A NICE SUBSET?
+
+# OKAY FOR ME IT MAKES SENSE TO HAVE A CLASS CALLED CARBON
+# AN ATTRIBUTE FOR EVERY POOL, THESE ATTRIBUTES ARE ONLY MODIFIED BY SPECIFIC FUNCTIONS
+
+
+class SoilCarbon:
+    """Class containing the full set of soil carbon pools.
+
+    TODO - Explain current contents + limitations
+    TODO - List attributes
+    """
+
+    def __init__(self) -> None:
+        """Initializes set of carbon pools."""
+        # TODO - ACTUALLY INIT POOLS HERE
+        pass
