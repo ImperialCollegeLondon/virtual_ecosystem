71fe9a3372db1fde6b0036746b781373113f3f1f
vgro <vivienne.groner@gmx.de>
2022-12-19 10:46:14 +0000

    ignore for now

--- virtual_rainforest/models/abiotic/__init__.py
+++ virtual_rainforest/models/abiotic/__init__.py
@@ -1,16 +1,16 @@
-import json
-from pathlib import Path
+# import json
+# from pathlib import Path
 
-from virtual_rainforest.core.config import register_schema
+# from virtual_rainforest.core.config import register_schema
 
 
-@register_schema("abiotic")
-def schema() -> dict:
-    """Defines the schema that the abiotic module configuration should conform to."""
-
-    schema_file = Path(__file__).parent.resolve() / "abiotic_schema.json"
-
-    with schema_file.open() as f:
-        config_schema = json.load(f)
-
-    return config_schema
+# @register_schema("abiotic")
+# def schema() -> dict:
+#    """Defines the schema that the abiotic module configuration should conform to."""
+#
+#    schema_file = Path(__file__).parent.resolve() / "abiotic_schema.json"
+#
+#    with schema_file.open() as f:
+#        config_schema = json.load(f)
+#
+#    return config_schema
