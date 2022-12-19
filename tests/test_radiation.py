32576a2cfc043988ac2ff6e64ac3f0a9a2966098
vgro <vivienne.groner@gmx.de>
2022-12-19 10:46:50 +0000

    added constants (tentative)

--- tests/test_radiation.py
+++ tests/test_radiation.py
@@ -1,13 +1,32 @@
 """Test module for abiotic.radiation.py."""
 
-# import pytest
+import pytest
+import numpy as np
 
-# from virtual_rainforest.models.abiotic import radiation
 # from core.constants import CONSTANTS as C  # this doesn't exist yet
+# from core.constants import CONSTANTS as C
+# this doesn't exist yet; optional scipy
+CLOUDY_TRANSMISSIVITY = 0.25  # cloudy transmittivity (Linacre, 1968)
+TRANSMISSIVITY_COEFFICIENT = (
+    0.50  # angular coefficient of transmittivity (Linacre, 1968)
+)
+FLUX_TO_ENERGY = 2.04  # from flux to energy conversion, umol/J (Meek et al., 1984)
+BOLZMAN_CONSTANT = 5.67 * 10 ** (-8)  # Stephan Bolzman constant W m-2 K-4
+SOIL_EMISSIVITY = 0.95  # default for tropical rainforest
+CANOPY_EMISSIVITY = 0.95  # default for tropical rainforest
+BEER_REGRESSION = 2.67e-5  # parameter in equation for atmospheric transmissivity based
+# on regression of Beer’s radiation extinction function (Allen 1996)
+ALBEDO_VIS = np.array(0.03, dtype=float)
+ALBEDO_SHORTWAVE = np.array(0.17, dtype=float)
+CELCIUS_TO_KELVIN = 273.15  # calculate absolute temperature in Kelvin
 
 
-def test_calc_ppfd(shortwave_in, sunshine_hours, albedo_vis):
+def test_calc_ppfd():
     """Test to be decided."""
+    from virtual_rainforest.models.abiotic import radiation
+    test = radiation.Radiation(elevation=100)
+    test.ppdf = test.calc_ppfd(shortwave_in=400, sunshine_hours=1.0, albedo_vis=ALBEDO_VIS)
+    assert test.ppfd == 
     raise NotImplementedError("Implementation of this feature is missing")
 
 
