422402d308a4b3ed19debf1ef7ba557f62d0df69
vgro <vivienne.groner@gmx.de>
2022-12-20 08:31:04 +0000

    file created

---
+++ virtual_rainforest/models/abiotic/energy_balance.py
@@ -0,0 +1,21 @@
+"""The `abiotic.energy_balance` module.
+
+This module calculates the energy balance for the Virtual Rainforest.
+
+The sequence of processes is based on Maclean et al, 2021: Microclimc: A mechanistic
+model of above, below and within-canopy microclimate. Ecological Modelling
+Volume 451, 109567. https://doi.org/10.1016/j.ecolmodel.2021.109567.
+"""
+
+# import numpy as np
+# from numpy.typing import NDArray
+
+# from core.constants import CONSTANTS as C
+
+
+class EnergyBalance:
+    """EnergyBalance method."""
+
+    def __init__(self) -> None:
+        """Initializes point-based energy_balance method."""
+        raise NotImplementedError("Implementation of this feature is still missing")
