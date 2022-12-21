f5d4bb08072a5407583a569432e107dbad1559c0
vgro <vivienne.groner@gmx.de>
2022-12-21 10:26:15 +0000

    test constant import added

--- tests/test_radiation.py
+++ tests/test_radiation.py
@@ -1,24 +1,36 @@
 """Test module for abiotic.radiation.py."""
 
-import numpy as np
 import pytest
 
 # from core.constants import CONSTANTS as C  # this doesn't exist yet
-# from core.constants import CONSTANTS as C
-# this doesn't exist yet; optional scipy
-CLOUDY_TRANSMISSIVITY = 0.25  # cloudy transmittivity (Linacre, 1968)
-TRANSMISSIVITY_COEFFICIENT = (
-    0.50  # angular coefficient of transmittivity (Linacre, 1968)
+from virtual_rainforest.models.abiotic.radiation import (
+    ALBEDO_SHORTWAVE,
+    ALBEDO_VIS,
+    BEER_REGRESSION,
+    BOLZMAN_CONSTANT,
+    CANOPY_EMISSIVITY,
+    CELCIUS_TO_KELVIN,
+    CLOUDY_TRANSMISSIVITY,
+    FLUX_TO_ENERGY,
+    SECOND_TO_DAY,
+    SOIL_EMISSIVITY,
+    TRANSMISSIVITY_COEFFICIENT,
 )
-FLUX_TO_ENERGY = 2.04  # from flux to energy conversion, umol/J (Meek et al., 1984)
-BOLZMAN_CONSTANT = 5.67 * 10 ** (-8)  # Stephan Bolzman constant W m-2 K-4
-SOIL_EMISSIVITY = 0.95  # default for tropical rainforest
-CANOPY_EMISSIVITY = 0.95  # default for tropical rainforest
-BEER_REGRESSION = 2.67e-5  # parameter in equation for atmospheric transmissivity based
-# on regression of Beer’s radiation extinction function (Allen 1996)
-ALBEDO_VIS = np.array(0.03, dtype=float)
-ALBEDO_SHORTWAVE = np.array(0.17, dtype=float)
-CELCIUS_TO_KELVIN = 273.15  # calculate absolute temperature in Kelvin
+
+
+def test_import_constants():
+    """Test that constants were imported correctly."""
+    assert CLOUDY_TRANSMISSIVITY == 0.25
+    assert TRANSMISSIVITY_COEFFICIENT == 0.50
+    assert FLUX_TO_ENERGY == 2.04
+    assert BOLZMAN_CONSTANT == 5.67e-8
+    assert SOIL_EMISSIVITY == 0.95
+    assert CANOPY_EMISSIVITY == 0.95
+    assert BEER_REGRESSION == 2.67e-5
+    assert ALBEDO_VIS == 0.03
+    assert ALBEDO_SHORTWAVE == 0.17
+    assert CELCIUS_TO_KELVIN == 273.15
+    assert SECOND_TO_DAY == 86400
 
 
 def test_calc_ppfd():
