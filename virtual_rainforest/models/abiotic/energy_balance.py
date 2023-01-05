4333aeb04ea0c550c6eaabbe62cad074722d3def
vgro <vivienne.groner@gmx.de>
2023-01-05 12:43:07 +0000

    prep functions added, not formatted

--- virtual_rainforest/models/abiotic/energy_balance.py
+++ virtual_rainforest/models/abiotic/energy_balance.py
@@ -6,13 +6,30 @@ model of above, below and within-canopy microclimate. Ecological Modelling
 Volume 451, 109567. https://doi.org/10.1016/j.ecolmodel.2021.109567.
 """
 
-# from math import exp as exp
+from math import exp, sqrt
 
 import numpy as np
 from numpy.typing import NDArray
 
 # from core.constants import CONSTANTS as C
+LEAF_TEMPERATURE_INI_FACTOR = 0.01  # factor used to initialise leaf temperature
+SOIL_DIVISION_FACTOR = 2.42  # factor defines how to divide soil into layers with
+# increasing thickness, alternateve value 1.2
+MIN_LEAF_CONDUCTIVITY = (
+    0.25  # typical for decidious forest with wind above canopy 2 m/s
+)
+MAX_LEAF_CONDUCTIVITY = (
+    0.32  # typical for decidious forest with wind above canopy 2 m/s
+)
+AIR_CONDUCTIVITY = 50.0  # typical for decidious forest with wind above canopy 2 m/s
+MIN_LEAF_AIR_CONDUCTIVITY = (
+    0.13  # typical for decidious forest with wind above canopy 2 m/s
+)
+MAX_LEAF_AIR_CONDUCTIVITY = (
+    0.19  # typical for decidious forest with wind above canopy 2 m/s
+)
 CELCIUS_TO_KELVIN = 273.15  # calculate absolute temperature in Kelvin
+STANDARD_MOLE = 44.6  # moles of ideal gas in 1 m^3 air at standard atmosphere
 VAPOR_PRESSURE_FACTOR1 = 0.6108  # constant in calculation of vapor pressure
 VAPOR_PRESSURE_FACTOR2 = 17.27  # constant in calculation of vapor pressure
 VAPOR_PRESSURE_FACTOR3 = 237.7  # constant in calculation of vapor pressure
@@ -23,10 +40,11 @@ data = {
     "air_temperature_2m": 25,
     "relative_humidity_2m": 90,
     "atmospheric_pressure_2m": 101.325,
-    "wind_speed_2m": 10.0,
+    "wind_speed_2m": 2.0,
+    "soil_moisture_reference": 0.3,
 }
 
-soil_parameters = {
+soil_parameters = {  # these are not real values
     "Sand": {
         "Smax": 1,  # Volumetric water content at saturation (m^3 / m^3)
         "Smin": 1,  # Residual water content (m^3 / m^3)
@@ -72,7 +90,7 @@ class EnergyBalance:
         height_of_above_canopy: NDArray[np.float32],
         relative_humidity_2m: NDArray[np.float32],
         air_temperature_2m: NDArray[np.float32],
-        mean_annual_temperature: NDArray[np.float32],
+        mean_annual_temperature: NDArray[np.float32], temperature of deepest soil layer
         atmospheric_pressure_2m: NDArray[np.float32],
         absorbed_radiation: : NDArray[np.float32],
         air_conductivity: NDArray[np.float32],
@@ -84,6 +102,8 @@ class EnergyBalance:
         diabatic_correction_factor: NDArray[np.float32]
         soil_type: NDArray[np.string_]
         soil_depth: NDArray[np.float32]
+        soil_moisture: NDArray[np.float32]
+        soil_moisture_reference: NDArray[np.float32]
     """
 
     def __init__(
@@ -107,12 +127,14 @@ class EnergyBalance:
         self.canopy_layers = canopy_layers
         self.soil_layers = soil_layers
         self.canopy_height = canopy_height
+        self.soil_depth = soil_depth
 
         self.air_temperature_2m = data["air_temperature_2m"]
         self.relative_humidity_2m = data["relative_humidity_2m"]
         self.atmospheric_pressure_2m = data["atmospheric_pressure_2m"]
         self.wind_speed_2m = data["wind_speed_2m"]
         self.mean_annual_temperature = mean_annual_temperature
+        self.soil_moisture = data["soil_moisture_reference"]
 
         # interpolate initial temperature profile, could be more realistic
         self.temperature_above_canopy = self.air_temperature_2m
@@ -129,7 +151,9 @@ class EnergyBalance:
             ::-1
         ]
         self.absorbed_radiation = absorbed_radiation
-        self.canopy_temperature = self.air_temperature + 0.01 * self.absorbed_radiation
+        self.canopy_temperature = (
+            self.air_temperature + LEAF_TEMPERATURE_INI_FACTOR * self.absorbed_radiation
+        )
 
         # initiate relative humidity and atmospheric pressure
         self.relative_humidity = np.repeat(
@@ -139,7 +163,7 @@ class EnergyBalance:
 
         # set initial conductivities
         self.air_conductivity = (
-            np.repeat(50, self.canopy_layers + 1)
+            np.repeat(AIR_CONDUCTIVITY, self.canopy_layers + 1)
             * (self.canopy_layers / self.canopy_height)
             * (2 / self.canopy_layers)
         )
@@ -151,10 +175,14 @@ class EnergyBalance:
         )
 
         self.leaf_conductivity = np.linspace(
-            np.array(0.35), np.array(0.32), self.canopy_layers
+            np.array(MIN_LEAF_CONDUCTIVITY),
+            np.array(MAX_LEAF_CONDUCTIVITY),
+            self.canopy_layers,
         )
         self.air_leaf_conductivity = np.linspace(
-            np.array(0.13), np.array(0.19), self.canopy_layers
+            np.array(MIN_LEAF_AIR_CONDUCTIVITY),
+            np.array(MAX_LEAF_AIR_CONDUCTIVITY),
+            self.canopy_layers,
         )
 
         # set initial heights
@@ -163,7 +191,9 @@ class EnergyBalance:
             for x in range(0, self.canopy_layers)
         ]
         self.soil_node_depths = [
-            2 / (self.soil_layers**2.42) * (x**2.42)
+            self.soil_depth
+            / (self.soil_layers**SOIL_DIVISION_FACTOR)
+            * (x**SOIL_DIVISION_FACTOR)
             for x in range(1, self.soil_layers + 1)
         ]
         self.height_of_above_canopy = self.canopy_height
@@ -180,21 +210,191 @@ class EnergyBalance:
 
         # get soil parameters
         self.soil_parameters = soil_parameters[str(soil_type)]
-        # there is another soil node depth in Maclean, not sure what the difference is
-        # self.soil_node_depth = [soil_depth/self.soil_layers**1.2 * (x**1.2)
-        # for x in range(1, self.soil_layers)]
+
+    def calc_molar_density_air(
+        self,
+        temperature: NDArray[np.float32] = 20.0,
+        atmospheric_pressure: NDArray[np.float32] = 101.3,
+    ) -> NDArray[np.float32]:
+        """Calculate temperature-dependent molar density of air.
+
+        Args:
+            temperature: NDArray[np.float32], temperature [C]
+            atmospheric_pressure: NDArray[np.float32], atmospheric pressure [kPa]
+
+        Returns:
+            molar_density_air: NDArray[np.float32], Molar density of air [mol m-3]
+        """
+        temperature_kelvin = temperature + CELCIUS_TO_KELVIN
+        molar_density_air = (
+            STANDARD_MOLE
+            * (temperature_kelvin / atmospheric_pressure)
+            * (CELCIUS_TO_KELVIN / temperature_kelvin)
+        )
+        return molar_density_air
+
+    def calc_specific_heat_air(
+        self, temperature: NDArray[np.float32] = 20.0
+    ) -> NDArray[np.float32]:
+        """Calculate molar temperature-dependent specific heat of air.
+
+        Args:
+            temperature: NDArray[np.float32], temperature [C]
+
+        Returns:
+            specific_heat_air: NDArray[np.float32], specific heat of air at constant
+                pressure (J mol-1 K-1)
+        """
+        specific_heat_air = 2e-05 * temperature**2 + 0.0002 * temperature + 29.119
+        return specific_heat_air
+
+    def calc_latent_heat_of_vaporisation(
+        self, temperature: NDArray[np.float32]
+    ) -> NDArray[np.float32]:
+        """Calculate latent heat of vaporisation.
+
+        Args:
+            temperature: NDArray[np.float32], temperature [C]
+
+        Returns:
+            Latent heat of vaporisation [J mol-1]
+        """
+        latent_heat_of_vaporisation = -42.575 * temperature + 44994
+        return latent_heat_of_vaporisation
+
+    def calc_roughness_length(
+        self,
+        canopy_height: NDArray[np.float32],
+        plant_area_index: NDArray[np.float32],
+        substrate_surface_drag_coefficient: float = 0.003,
+        height_scaling_parameter: float = 7.5,
+        roughness_element_drag_coefficient: float = 0.3,
+        roughness_sublayer_depth_coefficient: float = 0.193,
+        max_velocity_ratio: float = 0.3,
+    ) -> NDArray[np.float32]:
+        """Calculate roughness length governing momentum transfer.
+
+        Args:
+            canopy_height: NDArray[np.float32], canopy height [m]
+            plant_area_index: NDArray[np.float32], Plant area index [m m-1]
+            substrate_surface_drag_coefficient:float, substrate-surface drag coefficient
+            height_scaling_parameter: float, Control parameter scaling d/h (from Raupach
+                1994)
+            roughness_element_drag_coefficient: float,roughness-element drag coefficient
+            roughness_sublayer_depth_coefficient: float, parameter characterizes the
+                roughness sublayer depth
+            max_velocity_ratio: float, Max ratio of wind velocity to friction velocity
+
+        Returns:
+            roughness_length: NDArray[np.float32], momentum roughness length [m]
+
+        Reference:
+            Raupach, M.R. (1994) Simplified expressions for vegetation roughness length
+            and zero-plane displacement as functions of canopy height and area index.
+            Boundary-Layer Meteorology 71: 211-216.
+        """
+        zero_plane_displacement = (
+            1
+            - (1 - exp(-sqrt(height_scaling_parameter * plant_area_index)))
+            / sqrt(height_scaling_parameter * plant_area_index)
+        ) * canopy_height
+
+        wind_ratio = sqrt(
+            substrate_surface_drag_coefficient
+            + (roughness_element_drag_coefficient * plant_area_index) / 2
+        )
+        wind_ratio[wind_ratio > max_velocity_ratio] = max_velocity_ratio
+        roughness_length = (canopy_height - zero_plane_displacement) * exp(
+            -0.4 * (1 / wind_ratio) - roughness_sublayer_depth_coefficient
+        )
+        roughness_length = (
+            substrate_surface_drag_coefficient
+            if roughness_length < substrate_surface_drag_coefficient
+            else roughness_length
+        )
+        return roughness_length
+
+    # def calc_diabatic_correction_factor()
+
+    def calc_wind_profile(
+        self,
+        canopy_height: NDArray[np.float32],
+        plant_area_index: NDArray[np.float32],
+        height_scaling_parameter: float = 7.5,
+        ground_vegetation_height: float = 0.05,
+        diabatic_correction_factor: NDArray[np.float32] = 0.0,
+    ) -> None:
+        """Calculates wind speed at any given point above or below canopy.
+
+        Args:
+            canopy_height:NDArray[np.float32], height of canopy [m]
+            plant_area_index PAI plant area index [m m-1]
+            height_scaling_parameter: float = 7.5,
+            ground_vegetation_height: float = 0.05,
+
+            ui wind at reference height (m / s)
+            zi height of wind measurement (m)
+            zo height for which wind is required (m)
+            a=2 attenuation coefficient
+            diabatic_corretion_factor =0 diabatic correction factor
+            ground_vegetation_height: float=0.05,
+                height of ground vegetation layer below canopy (m)
+            zm0 = 0.004 roughness length (m) of vegetation layer below canopy as
+                returned by [microctools::roughlength()]
+
+        Returns:
+            wind_profile: NDArray[np.float32], wind speed in m s-1 at different heights
+        """
+        raise (NotImplementedError)
+        zero_plane_displacement = (
+            1
+            - (1 - exp(-sqrt(height_scaling_parameter * plant_area_index)))
+            / sqrt(height_scaling_parameter * plant_area_index)
+        ) * canopy_height
+        roughness_length = self.calc_roughness_length(canopy_height, plant_area_index)
+
+        dg = 0.65 * ground_vegetation_height
+        ground_vegetation_roughness_length = 0.1 * ground_vegetation_height
+
+    # ln1 = log((zi - zero_plane_displacement) / roughness_length) +
+    # diabatic_corretion_factor
+    # uf = 0.4 * (ui / ln1)
+    # zo above canopy
+    # ln2 = suppressWarnings(log((zo - zero_plane_displacement) / roughness_length) +
+    # diabatic_corretion_factor)
+    # ln2[ln2<0.55]<-0.55
+    # uo = (uf / 0.4) * ln2
+    # zo below canopy
+    # sel = which(zo < canopy_height)
+    # ln2 = log((canopy_height[sel] - zero_plane_displacement[sel]) /
+    # ground_vegetation_roughness_length[sel]) + diabatic_corretion_factor[sel]
+    # uo[sel] <- (uf[sel] / 0.4) * ln2
+    # zo above 10% of canopy hgt, but below top of canopy
+    # sel <- which(zo > (0.1 * canopy_height) & zo < canopy_height)
+    # uo[sel] <- uo[sel] * exp(a[sel] * ((zo[sel] / canopy_height[sel]) - 1))
+    # zo below 10% of canopy hgt
+    # sel <- which(zo <= (0.1 * canopy_height))
+    # uo[sel] <- uo[sel] * exp(a[sel] * (((0.1 * canopy_height[sel]) /
+    # canopy_height[sel]) - 1))
+    # ln1 <- log((0.1 * canopy_height[sel]) / ground_vegetation_roughness_length[sel])
+    # + diabatic_corretion_factor[sel]
+    # uf <- 0.4 * (uo[sel] / ln1)
+    # ln2 <- log(zo[sel] / ground_vegetation_roughness_length[sel])
+    # + diabatic_corretion_factor[sel]
+    # uo[sel] <- (uf / 0.4) * ln2
+    # uo
 
 
 # --- this is the sequence of processes in microclimc for one time step ---
 # paraminit - DONE
-# soilinit
+# soilinit - DONE
 # runonetimestep:
 # Check whether any vegetation layers have zero PAI
-# == Unpack climate variables ==
+# == Unpack climate variables == DONE
 # == calculate baseline variables: ==
-# - molar density of air
-# - specific heat of air
-# - latent heat of vaporisation
+# - molar density of air DONE
+# - specific heat of air DONE
+# - latent heat of vaporisation DONE
 # Adjust wind to 2 m above canopy
 # Generate heights of nodes
 # Set z above
