142db39fdc2646fe3d33945d2a8524742a791948
vgro <vivienne.groner@gmx.de>
2023-01-19 11:09:20 +0000

    attributes defined in docstrings, types removed

--- virtual_rainforest/models/abiotic/radiation.py
+++ virtual_rainforest/models/abiotic/radiation.py
@@ -70,24 +70,34 @@ class Radiation:
         self.elevation = elevation
         """Elevation above sea level, [m]"""
 
+        # populated later
+        self.ppfd = NDArray[np.float32]
+        """Daily top of canopy photosynthetic photon flux density, [mol m-2]"""
+        self.topofcanopy_radiation = NDArray[np.float32]
+        """Daily top of canopy downward shortwave radiation, [J m-2]"""
+        self.longwave_canopy = NDArray[np.float32]
+        """Daily longwave radiation from n individual canopy layers, [J m-2]"""
+        self.longwave_soil = NDArray[np.float32]
+        """Daily longwave radiation from soil, [J m-2]"""
+        self.netradiation_surface = NDArray[np.float32]
+        """Daily net shortwave radiation at the surface (= forest floor), [J m-2]"""
+
     def calc_ppfd(
         self,
         shortwave_in: NDArray[np.float32],
         sunshine_hours: NDArray[np.float32] = np.array(1.0, dtype=np.float32),
         albedo_vis: NDArray[np.float32] = ALBEDO_VIS,
     ) -> NDArray[np.float32]:
-        """Calculates photosynthetic photon flux density at the top of the canopy.
+        """Calculates top of canopy photosynthetic photon flux density [mol m-2].
 
         Args:
-            shortwave_in: NDArray[np.float32], daily downward shortwave radiation[J m-2]
-            sunshine_hours: NDArray[np.float32], fraction of sunshine hours, between 0
-                (100% cloud cover) and 1 (cloud free sky)
-            albedo_vis: NDArray[np.float32], visible light albedo, default = 0.03
+            shortwave_in: daily downward shortwave radiation [J m-2]
+            sunshine_hours: fraction of sunshine hours, between 0 (100% cloud cover)
+                and 1 (cloud free sky)
+            albedo_vis: visible light albedo, default = 0.03
 
         Returns:
-            tau: NDArray[np.float32], transmissivity
-            self.ppfd: NDArray[np.float32], daily top of canopy photosynthetic photon
-            flux density, [mol m-2]
+            tau: atmospheric transmissivity, unitless
 
         Reference:
             Davis et al. (2017): Simple process-led algorithms for simulating habitats
@@ -100,7 +110,6 @@ class Radiation:
 
         # Calculate daily photosynth. photon flux density (ppfd_d), mol/m^2
         self.ppfd = (1.0e-6) * FLUX_TO_ENERGY * (1.0 - albedo_vis) * tau * shortwave_in
-        """Daily top of canopy photosynthetic photon flux density, [mol m-2]"""
         return tau
 
     def calc_topofcanopy_radiation(
@@ -110,38 +119,28 @@ class Radiation:
         sunshine_hours: NDArray[np.float32] = np.array(1.0, dtype=np.float32),
         albedo_shortwave: NDArray[np.float32] = ALBEDO_SHORTWAVE,
     ) -> None:
-        """Calculate top of canopy shortwave radiation.
+        """Calculate top of canopy shortwave radiation [J m-2].
 
         Args:
-            shortwave_in: NDArray[np.float32], daily downward shortwave radiation[J m-2]
-            sunshine_hours: NDArray[np.float32], fraction of sunshine hours, between 0
-                (100% cloud cover) and 1 (cloud free sky)
-            albedo_shortwave: NDArray[np.float32], shortwave albedo, default = 0.17
-
-        Returns:
-            self.topofcanopy_radiation: NDArray[np.float32], top of canopy downward
-                shortwave radiation, [J m-2]
+            tau: atmospheric transmissivity, unitless
+            shortwave_in: daily downward shortwave radiation [J m-2]
+            sunshine_hours: fraction of sunshine hours, between 0 (100% cloud cover)
+                and 1 (cloud free sky)
+            albedo_shortwave: shortwave albedo, default = 0.17
         """
         self.topofcanopy_radiation = (1.0 - albedo_shortwave) * tau * shortwave_in
-        """Daily top of canopy downward shortwave radiation, [J m-2]"""
 
     def calc_longwave_radiation(
         self,
         canopy_temperature: NDArray[np.float32],
         surface_temperature: NDArray[np.float32],
     ) -> None:
-        """Calculates longwave emission from canopy and forest floor.
+        """Calculates longwave emission from canopy and forest floor [J m-2].
 
         Args:
-            canopy_temperature: NDArray[np.float32], canopy temperature of n layers [C];
-                the array size is set to max number of layers (n_max) and filled with
-                NaN where n < n_max
-            surface_temperature: NDArray[np.float32], surface soil temperature [C]
-
-        Returns:
-            self.longwave_canopy: NDArray[np.float32], longwave radiation from n
-                individual canopy layers [J m-2]
-            self.longwave_soil: NDArray[np.float32],longwave radiation from soil [J m-2]
+            canopy_temperature: canopy temperature of n layers [C]; the array size is
+                set to max number of layers (n_max) and filled with NaN where n < n_max
+            surface_temperature: surface soil temperature [C]
         """
         # longwave emission canopy
         self.longwave_canopy = (
@@ -149,7 +148,6 @@ class Radiation:
             * BOLZMAN_CONSTANT
             * (CELSIUS_TO_KELVIN + canopy_temperature) ** 4
         )
-        """Daily longwave radiation from n individual canopy layers, [J m-2]"""
 
         # longwave emission surface
         self.longwave_soil = (
@@ -157,20 +155,15 @@ class Radiation:
             * BOLZMAN_CONSTANT
             * (CELSIUS_TO_KELVIN + surface_temperature) ** 4
         )
-        """Daily longwave radiation from soil, [J m-2]"""
 
     def calc_netradiation_surface(
         self,
         canopy_absorption: NDArray[np.float32],
     ) -> None:
-        """Calculates daily net radiation at the forest floor.
+        """Calculates daily net radiation at the forest floor [J m-2].
 
         Args:
-            canopy_absorption: NDArray[np.float32]: absorption by canopy layers, [J m-2]
-
-        Returns:
-            self.netradiation_surface: NDArray[np.float32], net shortwave radiation at
-                the forest floor, [J m-2]
+            canopy_absorption: absorption by canopy layers, [J m-2]
         """
         self.netradiation_surface = (
             self.topofcanopy_radiation
@@ -178,7 +171,6 @@ class Radiation:
             - self.longwave_soil
             - self.longwave_canopy  # np.sum(self.longwave_canopy, axis=1)
         )
-        """Daily net shortwave radiation at the surface (= forest floor), [J m-2]"""
 
     def radiation_balance(
         self,
@@ -193,28 +185,25 @@ class Radiation:
     ) -> None:
         """Calculate radiation balance.
 
-        Args:
-            elevation: NDArray[np.float32], elevation [m]
-            shortwave_in: NDArray[np.float32], daily downward shortwave radiation[J m-2]
-            sunshine_hours: NDArray[np.float32], fraction of sunshine hours, between 0
-                (100% cloud cover) and 1 (cloud free sky)
-            albedo_vis: NDArray[np.float32], visible light albedo, default = 0.03
-            albedo_shortwave: NDArray[np.float32], shortwave albedo, default = 0.17
-            canopy_temperature: NDArray[np.float32], canopy temperature of n layers [C]
-            surface_temperature: NDArray[np.float32], surface soil temperature [C]
-            canopy_absorption: NDArray[np.float32]: absorption by canopy [J m-2]
+        The function runs the full radiation balance and populates the following
+        attributes:
+            * elevation [m]
+            * top of canopy downward shortwave radiation, [J m-2]
+            * top of canopy photosynthetic photon flux density,[mol m-2]
+            * longwave radiation from n individual canopy layers [J m-2]
+            * longwave radiation from soil [J m-2]
+            * net shortwave radiation at the surface (=forest floor)[J m-2]
 
-        Returns:
-            elevation: NDArray[np.float32], elevation [m]
-            topofcanopy_radiation: NDArray[np.float32], top of canopy downward shortwave
-                radiation, [J m-2]
-            ppfd: NDArray[np.float32], top of canopy photosynthetic photon flux density,
-                [mol m-2]
-            longwave_canopy: NDArray[np.float32], longwave radiation from n individual
-                canopy layers [J m-2]
-            longwave_soil: NDArray[np.float32], longwave radiation from soil [J m-2]
-            netradiation_surface: NDArray[np.float32], net shortwave radiation at
-                the surface (=forest floor)[J m-2]
+        Args:
+            elevation: elevation [m]
+            shortwave_in: daily downward shortwave radiation[J m-2]
+            sunshine_hours: fraction of sunshine hours, between 0
+                (100% cloud cover) and 1 (cloud free sky)
+            albedo_vis: visible light albedo, default = 0.03
+            albedo_shortwave: shortwave albedo, default = 0.17
+            canopy_temperature: canopy temperature of n layers [C]
+            surface_temperature: surface soil temperature [C]
+            canopy_absorption: absorption by canopy [J m-2]
         """
 
         tau = self.calc_ppfd(shortwave_in, sunshine_hours, albedo_vis)
