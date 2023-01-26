630650bf186b75e2e5b8bc560abc9f7a4bb37cba
dorme <d.orme@imperial.ac.uk>
2023-01-26 12:02:54 +0000

    Updated test_grid to remove resting of removed strict option

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -382,7 +382,6 @@ def test_map_xy_to_cell_ids(
         "y_coord",
         "x_idx",
         "y_idx",
-        "strict",
         "exp_exception",
         "exp_message",
         "exp_idx",
@@ -393,19 +392,8 @@ def test_map_xy_to_cell_ids(
             np.array([0, 1, 2]),
             None,
             None,
-            True,
             pytest.raises(ValueError),
             "Mapped points fall outside grid.",
-            None,
-        ),
-        (
-            np.array([0, 1, 2]),
-            np.array([0, 1, 2]),
-            None,
-            None,
-            False,  # strict changes error type
-            pytest.raises(ValueError),
-            "Mapped points do not cover all cells.",
             None,
         ),
         (
@@ -413,7 +401,6 @@ def test_map_xy_to_cell_ids(
             np.array([200000, 200100, 200200]),
             None,
             None,
-            True,
             pytest.raises(ValueError),
             "Mapped points fall on cell boundaries.",
             None,
@@ -423,7 +410,6 @@ def test_map_xy_to_cell_ids(
             np.array([200050, 200150, 200250]),
             None,
             None,
-            True,
             pytest.raises(ValueError),
             "Mapped points do not cover all cells.",
             None,
@@ -433,7 +419,6 @@ def test_map_xy_to_cell_ids(
             np.repeat(np.arange(200950, 200000, -100), 10),
             None,
             None,
-            True,
             does_not_raise(),
             None,
             tuple([np.arange(100), np.arange(100)]),
@@ -443,7 +428,6 @@ def test_map_xy_to_cell_ids(
             np.repeat(np.arange(200950, 200000, -100), 10),
             np.arange(50),
             np.arange(50),
-            True,
             pytest.raises(ValueError),
             "Dimensions of x/y indices do not match coordinates",
             None,
@@ -453,7 +437,6 @@ def test_map_xy_to_cell_ids(
             np.repeat(np.arange(200950, 200000, -100), 10),
             np.arange(50),
             None,
-            True,
             pytest.raises(ValueError),
             "Only one of x/y indices provided.",
             None,
@@ -463,7 +446,6 @@ def test_map_xy_to_cell_ids(
             np.repeat(np.arange(200950, 200000, -100), 10),
             np.arange(100),
             np.arange(100),
-            True,
             does_not_raise(),
             None,
             tuple([np.arange(100), np.arange(100)]),
@@ -473,7 +455,6 @@ def test_map_xy_to_cell_ids(
             np.concatenate([np.repeat(np.arange(200950, 200000, -100), 10), [200050]]),
             None,
             None,
-            True,
             pytest.raises(ValueError),
             "Some cells contain more than one point.",
             None,
@@ -483,20 +464,9 @@ def test_map_xy_to_cell_ids(
             np.repeat(np.arange(200950, 199900, -100), 10),
             None,
             None,
-            True,
             pytest.raises(ValueError),
             "Mapped points fall outside grid.",
             None,
-        ),
-        (  # Grid covers but extends outside cells and strict = False
-            np.tile(np.arange(500050, 501000, 100), 11),
-            np.repeat(np.arange(200950, 199900, -100), 10),
-            None,
-            None,
-            False,
-            does_not_raise(),
-            None,
-            tuple([np.arange(100), np.arange(100)]),
         ),
     ],
 )
@@ -509,7 +479,6 @@ def test_map_xy_to_cell_indexing(
     exp_exception,
     exp_message,
     exp_idx,
-    strict,
 ):
     """Test coordinate checking.
 
@@ -519,7 +488,7 @@ def test_map_xy_to_cell_indexing(
     with exp_exception as excep:
 
         x_idx, y_idx = fixture_square_grid.map_xy_to_cell_indexing(
-            x_coord, y_coord, x_idx, y_idx, strict=strict
+            x_coord, y_coord, x_idx, y_idx
         )
 
     if exp_message is not None:
@@ -531,14 +500,13 @@ def test_map_xy_to_cell_indexing(
 
 
 @pytest.mark.parametrize(
-    argnames=["x_coord", "y_coord", "x_idx", "y_idx", "strict", "exp_x", "exp_y"],
+    argnames=["x_coord", "y_coord", "x_idx", "y_idx", "exp_x", "exp_y"],
     argvalues=[
         (
             np.array([1, 2, 1, 2]),
             np.array([2, 2, 1, 1]),
             None,
             None,
-            True,
             np.array([0, 1, 2, 3]),
             np.array([0, 1, 2, 3]),
         ),
@@ -547,32 +515,13 @@ def test_map_xy_to_cell_indexing(
             np.array([2, 2, 1, 1]),
             np.array([0, 1, 0, 1]),
             np.array([0, 0, 1, 1]),
-            True,
             np.array([0, 1, 0, 1]),
             np.array([0, 0, 1, 1]),
-        ),
-        (
-            np.array([0, 1, 2, 0, 1, 2, 0, 1, 2]),
-            np.array([2, 2, 2, 1, 1, 1, 0, 0, 0]),
-            None,
-            None,
-            False,
-            np.array([1, 2, 4, 5]),
-            np.array([1, 2, 4, 5]),
-        ),
-        (
-            np.array([0, 1, 2, 0, 1, 2, 0, 1, 2]),
-            np.array([2, 2, 2, 1, 1, 1, 0, 0, 0]),
-            np.array([0, 1, 2, 0, 1, 2, 0, 1, 2]),
-            np.array([2, 2, 2, 1, 1, 1, 0, 0, 0]),
-            False,
-            np.array([1, 2, 1, 2]),
-            np.array([2, 2, 1, 1]),
         ),
     ],
 )
 def test_map_xy_to_cell_indexing_return(
-    fixture_square_grid_simple, x_coord, y_coord, x_idx, y_idx, strict, exp_x, exp_y
+    fixture_square_grid_simple, x_coord, y_coord, x_idx, y_idx, exp_x, exp_y
 ):
     """Test xy to cell id index return values.
 
@@ -580,7 +529,7 @@ def test_map_xy_to_cell_indexing_return(
     """
 
     x_idx, y_idx = fixture_square_grid_simple.map_xy_to_cell_indexing(
-        x_coord, y_coord, x_idx, y_idx, strict=strict
+        x_coord, y_coord, x_idx, y_idx
     )
 
     assert np.allclose(exp_x, x_idx)
