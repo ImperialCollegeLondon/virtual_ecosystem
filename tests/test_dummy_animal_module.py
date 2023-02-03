e0201f3febf2ab338d977e5fb77505b37eae8aaa
Taran Rallings <tralling@ic.ac.uk>
2023-02-03 17:12:43 +0000

    Reworked grow() tests to be parameterized, put VR imports inside tests, and added an init test for the soil pool.

--- tests/test_dummy_animal_module.py
+++ tests/test_dummy_animal_module.py
@@ -3,38 +3,34 @@
 This module tests the functionality of dummy_animal_module.py
 """
 
-import virtual_rainforest.models.animals.dummy_animal_module as am
+import pytest
+
+
+@pytest.fixture
+def plant_instance():
+    """Fixutre for a plant community used in tests."""
+    from virtual_rainforest.models.animals.dummy_animal_module import PlantCommunity
+
+    return PlantCommunity(10.0, 1)
 
 
 class TestPlantCommunity:
     """Test Plant class."""
 
-    def test_grow1(self):
-        """Testing grow at 100% energy."""
-        p = am.PlantCommunity(10.0, 1)
-        p.grow()
-        assert p.energy == 10000.0
+    @pytest.mark.parametrize(
+        "initial, final", [(10000.0, 10000.0), (500.0, 975.0), (0.0, 0.0)]
+    )
+    def test_grow(self, plant_instance, initial, final):
+        """Testing grow at 100%, 50%, and 0% maximum energy."""
+        plant_instance.energy = initial
+        plant_instance.grow()
+        assert plant_instance.energy == final
 
-    def test_grow2(self):
-        """Testing grow at 50% energy."""
-        p = am.PlantCommunity(10.0, 1)
-        p.energy = 500.0
-        p.grow()
-        assert p.energy == 975.0
-
-    def test_grow3(self):
-        """Testing grow at 0% energy."""
-        p = am.PlantCommunity(10.0, 1)
-        p.energy = 0.0
-        p.grow()
-        assert p.energy == 0.0
-
-    def test_die(self):
+    def test_die(self, plant_instance):
         """Testing die."""
-        p = am.PlantCommunity(10.0, 1)
-        if p.is_alive:
-            p.die()
-        assert not p.is_alive
+        if plant_instance.is_alive:
+            plant_instance.die()
+            assert not plant_instance.is_alive
 
 
 class TestCarcassPool:
@@ -42,5 +38,18 @@ class TestCarcassPool:
 
     def test_initialization(self):
         """Testing initialization of CarcassPool."""
-        c1 = am.CarcassPool(1000.7, 1)
+        from virtual_rainforest.models.animals.dummy_animal_module import CarcassPool
+
+        c1 = CarcassPool(1000.7, 1)
         assert c1.energy == 1000.7
+
+
+class TestSoilPool:
+    """Test the Soil Pool class."""
+
+    def test_initialization(self):
+        """Testing initialization of soil pool."""
+        from virtual_rainforest.models.animals.dummy_animal_module import SoilPool
+
+        s1 = SoilPool(1000.7, 1)
+        assert s1.energy == 1000.7
