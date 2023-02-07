cd685ef683c7e3e2606ed9bbd00ba14ad4232dd5
Taran Rallings <tralling@ic.ac.uk>
2023-02-07 10:27:18 +0000

    Removed files that, I think, were creating merge conflicts with feature/dummy_animals.

--- virtual_rainforest/animals/dummy_animal_module.py
+++
@@ -1,8 +0,0 @@
-"""The 'dummy_animal_module.
-
-This file provides toy animal module functionality as well as self-contained
-dummy versions of the abiotic, soil, and plant modules that are required for
-setting up and testing the early stages of the animal module.
-"""
-
-# experimental file for figuring out how to make an animal module
