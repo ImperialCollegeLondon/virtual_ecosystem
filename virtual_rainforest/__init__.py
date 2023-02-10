04c6e84a0e98de781910088832723e84a1bae7ff
David Orme <davidorme@users.noreply.github.com>
2023-02-10 10:52:27 +0000

    Restored import of core schema to vr.__init__.py

--- virtual_rainforest/__init__.py
+++ virtual_rainforest/__init__.py
@@ -2,9 +2,12 @@ import importlib.metadata
 import pkgutil
 from importlib import import_module
 
-# Autodiscover models in the models module
 import virtual_rainforest.models as vfm
 
+# Import the core schema to register it
+from virtual_rainforest.core import schema  # noqa: F401
+
+# Autodiscover models in the models module to add their schema and BaseModel subclass
 for module_info in pkgutil.iter_modules(vfm.__path__):
     import_module(f"virtual_rainforest.models.{module_info.name}")
 
