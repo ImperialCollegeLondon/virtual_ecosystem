1dc3990786e3c089bf07a71ed77c33c94db67823
David Orme <davidorme@users.noreply.github.com>
2023-02-10 10:58:31 +0000

    More explanatory comments in __init__.py

--- virtual_rainforest/__init__.py
+++ virtual_rainforest/__init__.py
@@ -8,6 +8,10 @@ import virtual_rainforest.models as vfm
 from virtual_rainforest.core import schema  # noqa: F401
 
 # Autodiscover models in the models module to add their schema and BaseModel subclass
+# All modules within virtual_rainforest.model are expected to:
+# - import their BaseModel subclass to the module root, for example:
+#   from virtual_rainforest.models.soil.model import SoilModel  # noqa: F401
+# - define a schema member, decorated with @register_schema
 for module_info in pkgutil.iter_modules(vfm.__path__):
     import_module(f"virtual_rainforest.models.{module_info.name}")
 
