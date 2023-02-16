d04f626f71779edbcc2191ef655ba3b154b810ca
David Orme <davidorme@users.noreply.github.com>
2023-02-16 14:01:27 +0000

    Typo in soil schema name

--- virtual_rainforest/models/soil/__init__.py
+++ virtual_rainforest/models/soil/__init__.py
@@ -6,4 +6,4 @@ from virtual_rainforest.models.soil.soil_model import SoilModel  # noqa: F401
 with resources.path(
     "virtual_rainforest.models.soil", "soil_schema.json"
 ) as schema_file_path:
-    register_schema(module_name="abiotic", schema_file_path=schema_file_path)
+    register_schema(module_name="soil", schema_file_path=schema_file_path)
