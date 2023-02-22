2d3b1dafb1e6443aeb99ef1ed79855ce39b85391
Taran Rallings <tralling@ic.ac.uk>
2023-02-22 15:56:51 +0000

    Changed model naming in init.py.

--- virtual_rainforest/models/animals/__init__.py
+++ virtual_rainforest/models/animals/__init__.py
@@ -16,9 +16,11 @@ carcasses.
 from importlib import resources
 
 from virtual_rainforest.core.config import register_schema
-from virtual_rainforest.models.animals.animal_model import AnimalModel  # noqa: F401
+from virtual_rainforest.models.animals.animal_model import AnimalModel
 
 with resources.path(
     "virtual_rainforest.models.animals", "animals_schema.json"
 ) as schema_file_path:
-    register_schema(module_name="animal", schema_file_path=schema_file_path)
+    register_schema(
+        module_name=AnimalModel.model_name, schema_file_path=schema_file_path
+    )
