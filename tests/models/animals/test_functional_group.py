96a06ad63c5beeae2712880e18b9430c2c0c2579
Taran Rallings <tralling@ic.ac.uk>
2023-03-23 15:37:55 +0000

    Fixed the testing for FunctionalGroup.

--- tests/models/animals/test_functional_group.py
+++ tests/models/animals/test_functional_group.py
@@ -7,19 +7,21 @@ class TestFunctionalGroup:
     """Test Animal class."""
 
     @pytest.mark.parametrize(
-        "taxa, diet, dam_law_exp, dam_law_coef, conv_eff",
+        "name, taxa, diet, dam_law_exp, dam_law_coef, conv_eff",
         [
-            ("mammal", "herbivore", -0.75, 4.23, 0.1),
-            ("mammal", "carnivore", -0.75, 1.00, 0.25),
-            ("bird", "herbivore", -0.75, 5.00, 0.1),
-            ("bird", "carnivore", -0.75, 2.00, 0.25),
+            ("herbivorous_mammal", "mammal", "herbivore", -0.75, 4.23, 0.1),
+            ("carnivorous_mammal", "mammal", "carnivore", -0.75, 1.00, 0.25),
+            ("herbivorous_bird", "bird", "herbivore", -0.75, 5.00, 0.1),
+            ("carnivorous_bird", "bird", "carnivore", -0.75, 2.00, 0.25),
         ],
     )
-    def test_initialization(self, taxa, diet, dam_law_exp, dam_law_coef, conv_eff):
+    def test_initialization(
+        self, name, taxa, diet, dam_law_exp, dam_law_coef, conv_eff
+    ):
         """Testing initialization of derived parameters for animal cohorts."""
         from virtual_rainforest.models.animals.functional_group import FunctionalGroup
 
-        func_group = FunctionalGroup(taxa, diet)
+        func_group = FunctionalGroup(name, taxa, diet)
         assert func_group.damuths_law_terms[0] == dam_law_exp
         assert func_group.damuths_law_terms[1] == dam_law_coef
         assert func_group.conversion_efficiency == conv_eff
