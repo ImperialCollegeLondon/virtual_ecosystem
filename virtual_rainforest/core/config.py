35f277ec363d44c150343af4cb0f5a46f1b45f7f
Jacob Cook <jc2017@ic.ac.uk>
2023-03-23 11:36:57 +0000

    Removed probably unneccesary TODO

--- virtual_rainforest/core/config.py
+++ virtual_rainforest/core/config.py
@@ -242,7 +242,6 @@ def check_dict_leaves(
     return conflicts
 
 
-# TODO - MOVE THIS TO UTILS MODULE (ONCE THAT IS MERGED)
 def check_outfile(merge_file_path: Path) -> None:
     """Check that final output file is not already in the output folder.
 
