a5d736b6a71411feb79d482b06248785c25a2bda
Jacob Cook <jc2017@ic.ac.uk>
2023-03-27 08:56:13 +0100

    Made check_out file a core.utils function

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -41,57 +41,6 @@ def test_check_dict_leaves(d_a: dict, d_b: dict, overlap: list) -> None:
 
 
 @pytest.mark.parametrize(
-    "out_path,expected_log_entries",
-    [
-        (
-            "./complete_config.toml",
-            (
-                (
-                    CRITICAL,
-                    "A file in the user specified output folder (.) already makes use "
-                    "of the specified output file name (complete_config.toml), this "
-                    "file should either be renamed or deleted!",
-                ),
-            ),
-        ),
-        (
-            "bad_folder/complete_config.toml",
-            (
-                (
-                    CRITICAL,
-                    "The user specified output directory (bad_folder) doesn't exist!",
-                ),
-            ),
-        ),
-        (
-            "pyproject.toml/complete_config.toml",
-            (
-                (
-                    CRITICAL,
-                    "The user specified output folder (pyproject.toml) isn't a "
-                    "directory!",
-                ),
-            ),
-        ),
-    ],
-)
-def test_check_outfile(caplog, mocker, out_path, expected_log_entries):
-    """Check that an error is logged if an output file is already saved."""
-    from virtual_rainforest.core.config import check_outfile
-
-    # Configure the mock to return a specific list of files
-    if out_path == "./complete_config.toml":
-        mock_content = mocker.patch("virtual_rainforest.core.config.Path.exists")
-        mock_content.return_value = True
-
-    # Check that check_outfile fails as expected
-    with pytest.raises(ConfigurationError):
-        check_outfile(Path(out_path))
-
-    log_check(caplog, expected_log_entries)
-
-
-@pytest.mark.parametrize(
     "cfg_paths,contents,expected_exception,expected_log_entries",
     [
         (
--- virtual_rainforest/core/config.py
+++ virtual_rainforest/core/config.py
@@ -40,6 +40,7 @@ from jsonschema import Draft202012Validator, FormatChecker, exceptions, validato
 
 from virtual_rainforest.core.exceptions import ConfigurationError
 from virtual_rainforest.core.logger import LOGGER
+from virtual_rainforest.core.utils import check_outfile
 
 if sys.version_info[:2] >= (3, 11):
     import tomllib
@@ -235,54 +236,6 @@ def check_dict_leaves(
                 conflicts.append("%s" % ".".join(path + [str(key)]))
 
     return conflicts
-
-
-def check_outfile(merge_file_path: Path) -> None:
-    """Check that final output file is not already in the output folder.
-
-    Args:
-        merge_file_path: Path to save merged config file to (i.e. folder location + file
-            name)
-
-    Raises:
-        ConfigurationError: If the final output directory doesn't exist, isn't a
-            directory, or the final output file already exists.
-    """
-
-    # Extract parent folder name and output file name. If this is a relative path, it is
-    # expected to be relative to where the command is being run.
-    if not merge_file_path.is_absolute():
-        parent_fold = merge_file_path.parent.relative_to(".")
-    else:
-        parent_fold = merge_file_path.parent
-    out_file_name = merge_file_path.name
-
-    # Throw critical error if the output folder doesn't exist
-    if not Path(parent_fold).exists():
-        to_raise = ConfigurationError(
-            f"The user specified output directory ({parent_fold}) doesn't exist!"
-        )
-        LOGGER.critical(to_raise)
-        raise to_raise
-
-    elif not Path(parent_fold).is_dir():
-        to_raise = ConfigurationError(
-            f"The user specified output folder ({parent_fold}) isn't a directory!"
-        )
-        LOGGER.critical(to_raise)
-        raise to_raise
-
-    # Throw critical error if combined output file already exists
-    if merge_file_path.exists():
-        to_raise = ConfigurationError(
-            f"A file in the user specified output folder ({parent_fold}) already "
-            f"makes use of the specified output file name ({out_file_name}), this "
-            f"file should either be renamed or deleted!"
-        )
-        LOGGER.critical(to_raise)
-        raise to_raise
-
-    return None
 
 
 def collect_files(cfg_paths: list[str]) -> list[Path]:
