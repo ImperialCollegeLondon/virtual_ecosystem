26207ab57d327f9cf29aae07e2abdeb8a67a7ffc
Jacob Cook <jc2017@ic.ac.uk>
2023-04-25 15:47:58 +0100

    Enforced fixed constants for soil module

--- virtual_rainforest/models/soil/constants.py
+++ virtual_rainforest/models/soil/constants.py
@@ -4,14 +4,12 @@ constants" (fitting relationships taken from the literature) required by the bro
 """  # noqa: D205, D415
 
 from dataclasses import dataclass
-
-# TODO - Need to check for the sources of all constants defined here as part of checking
-# whether Abramoff et al.'s parameterisation works
+from typing import Final
 
 # TODO - Need to figure out a sensible area to volume conversion
 
 
-@dataclass
+@dataclass(frozen=True)
 class BindingWithPH:
     """From linear regression :cite:p:`mayes_relation_2012`."""
 
@@ -21,7 +19,7 @@ class BindingWithPH:
     """Unit of log(m^3 kg^-1). n.b. +3 converts from mg^-1 to kg^-1 and L to m^3"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class MaxSorptionWithClay:
     """From linear regression :cite:p:`mayes_relation_2012`."""
 
@@ -31,7 +29,7 @@ class MaxSorptionWithClay:
     """Unit of log(kg C kg soil ^-1). n.b. -6 converts from mg to kg"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class MoistureScalar:
     """Used in :cite:t:`abramoff_millennial_2018`, can't find original source."""
 
@@ -41,7 +39,7 @@ class MoistureScalar:
     """Units of (RWC)^-1"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class TempScalar:
     """Used in :cite:t:`abramoff_millennial_2018`, can't find original source."""
 
@@ -57,26 +55,26 @@ class TempScalar:
     """Reference temperature [degrees C]"""
 
 
-MICROBIAL_TURNOVER_RATE = 0.036
+MICROBIAL_TURNOVER_RATE: Final[float] = 0.036
 """Microbial turnover rate [day^-1], this isn't a constant but often treated as one."""
 
-MAX_UPTAKE_RATE_LABILE_C = 0.35
+MAX_UPTAKE_RATE_LABILE_C: Final[float] = 0.35
 """Maximum (theoretical) rate at which microbes can take up labile carbon [day^-1]."""
 
-NECROMASS_ADSORPTION_RATE = 0.025
+NECROMASS_ADSORPTION_RATE: Final[float] = 0.025
 """Rate at which necromass is adsorbed by soil minerals [day^-1].
 
 Taken from :cite:t:`abramoff_millennial_2018`, where it was obtained by calibration.
 """
 
-HALF_SAT_MICROBIAL_ACTIVITY = 0.0072
+HALF_SAT_MICROBIAL_ACTIVITY: Final[float] = 0.0072
 """Half saturation constant for microbial activity (with increasing biomass)[kg C m^-2].
 """
 
-LEACHING_RATE_LABILE_CARBON = 1.5e-3
+LEACHING_RATE_LABILE_CARBON: Final[float] = 1.5e-3
 """Leaching rate for labile carbon (lmwc) [day^-1]."""
 
-CARBON_INPUT_TO_POM = 2.0 / 3.0
+CARBON_INPUT_TO_POM: Final[float] = 2.0 / 3.0
 """Proportion of carbon input that becomes particulate organic matter (POM) [unitless].
 
 Taken from :cite:t:`abramoff_millennial_2018`, this is justified there based on previous
@@ -84,7 +82,7 @@ empirical work. However, this is something we will definitely completely alter d
 line so no need to worry too much about references.
 """
 
-LITTER_INPUT_RATE = 0.172 / 365.25
+LITTER_INPUT_RATE: Final[float] = 0.172 / 365.25
 """Rate of litter input to the system [kg C m^-2 day^-1].
 
 This definitely is not a constant for our purposes. However,
@@ -93,7 +91,7 @@ use one initially.
 """
 
 
-@dataclass
+@dataclass(frozen=True)
 class CarbonUseEfficiency:
     """Collection of carbon use efficiency parameters.
 
