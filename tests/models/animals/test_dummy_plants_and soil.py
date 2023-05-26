9a3bf9436258d69bf62c68d76c07c01b9d7f61f0
Taran Rallings <tralling@ic.ac.uk>
2023-05-26 16:28:13 +0100

    I found yet additional things i missed.

--- tests/models/animals/test_dummy_plants_and soil.py
+++ tests/models/animals/test_dummy_plants_and soil.py
@@ -34,7 +34,7 @@ class TestPlantCommunity:
 @pytest.fixture
 def soil_instance():
     """Fixture for a soil pool used in tests."""
-    from virtual_rainforest.models.animals.animal_module import PalatableSoil
+    from virtual_rainforest.models.animals.dummy_plants_and_soil import PalatableSoil
 
     return PalatableSoil(100000.0, 4)
 
