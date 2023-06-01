8ff88ecc3802aa03df8bcf88ba7c668d250bf635
Taran Rallings <tralling@ic.ac.uk>
2023-06-01 15:38:41 +0100

    Added remaining submodule references to __init__.py.

--- virtual_rainforest/models/animals/__init__.py
+++ virtual_rainforest/models/animals/__init__.py
@@ -7,10 +7,22 @@ Each of the animal sub-modules has its own API reference page:
   AnimalModel class which consolidates the functionality of the animal module
   into a single class, which the high level functions of the Virtual Rainforest
   can then make use of.
-* The :mod:`~virtual_rainforest.models.animals.carcasses` provides a model for the
-  surface carcasses.
+* The :mod:`~virtual_rainforest.models.animals.animal_communities` provides a class for
+  containing and managing all of the animal cohorts within a grid square.
+* The :mod:`~virtual_rainforest.models.animals.animal_cohorts` provides a class for the
+  individual animal cohorts, their attributes, and behaviors.
+* The :mod:`~virtual_rainforest.models.animals.functional_group` provides a class for
+  the animal functional groups that define the type of animal in an animal cohort.
+* The :mod:`~virtual_rainforest.models.animals.scaling_functions` provides a set of
+  allometric scaling functions that define the biological rates used in the animal
+  module.
 * The :mod:`~virtual_rainforest.models.animals.constants` provides a set of dataclasses
-  containing the constants required by the broader animal model. (to come)
+  containing the constants required by the broader animal model.
+* The :mod:`~virtual_rainforest.models.animals.carcasses` provides a model for the
+  surface carcasses created by mortality.
+* The :mod:`~virtual_rainforest.models.animals.dummy_plants_and_soil` provides a set of
+  classes defining toy implementations of soil and plant models that aid development of
+  the animal module.
 """  # noqa: D205, D415
 
 from importlib import resources
