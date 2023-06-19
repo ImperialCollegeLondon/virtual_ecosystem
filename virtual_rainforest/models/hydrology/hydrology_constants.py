14c2a54d9d79b41552ac60fc1fc28498bb15a0a1
vgro <vivienne.groner@gmx.de>
2023-06-19 09:09:11 +0100

    removed unnecessary bounds

---
+++ virtual_rainforest/models/hydrology/hydrology_constants.py
@@ -0,0 +1,14 @@
+"""The :mod:`~virtual_rainforest.models.hydrology.hydrology_constants` module contains
+constants and paratemters for the hydrology model. This is a temporaty solution.
+"""  # noqa: D205, D415
+
+from typing import Dict
+
+HydrologyParameters: Dict[str, float] = {
+    "soil_moisture_capacity": 0.9,
+    "water_interception_factor": 0.1,
+}
+"""Parameters for hydrology model."""
+
+# TODO move bounds to core.bound_checking once that is implemented and introduce method
+# to conserve energy and matter
