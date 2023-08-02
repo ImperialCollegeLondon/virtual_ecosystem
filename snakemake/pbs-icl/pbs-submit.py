e3ddbff21e06f00e707f9e398dfd71fca4ca2bb1
Alex Dewar <a.dewar@imperial.ac.uk>
2023-08-02 12:21:42 +0100

    Add support for setting runtime

--- snakemake/pbs-icl/pbs-submit.py
+++ snakemake/pbs-icl/pbs-submit.py
@@ -5,7 +5,13 @@ import sys
 from os import execvp
 
 
-def main(mem_mb: int, job_script: str) -> None:
+def get_runtime_str(runtime_min: int) -> str:
+    """Convert the runtime in whole minutes to HH:MM:SS."""
+    hours, mins = divmod(runtime_min, 60)
+    return f"{hours:02}:{mins:02}:00"
+
+
+def main(mem_mb: int, runtime_min: int, job_script: str) -> None:
     """The main entry point to this script."""
     # qsub wants memory requirement in whole gigabytes
     mem_gb = max(1, math.ceil(mem_mb / 1000))
@@ -14,7 +20,7 @@ def main(mem_mb: int, job_script: str) -> None:
         "qsub",
         (
             "qsub",
-            "-lwalltime=00:05:00",
+            f"-lwalltime={get_runtime_str(runtime_min)}",
             f"-lselect=1:ncpus=1:mem={mem_gb}gb",
             job_script,
         ),
@@ -23,5 +29,5 @@ def main(mem_mb: int, job_script: str) -> None:
 
 if __name__ == "__main__":
     # TODO: Display help if args not given
-    assert len(sys.argv) == 3
-    main(int(sys.argv[1]), sys.argv[2])
+    assert len(sys.argv) == 4
+    main(int(sys.argv[1]), int(sys.argv[2]), sys.argv[-1])
