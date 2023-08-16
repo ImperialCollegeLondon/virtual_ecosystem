9ccb64dea866a3eec43f0103ce5dba956c131b04
Jacob Cook <jc2017@ic.ac.uk>
2023-08-16 10:27:40 +0100

    Added test for the excrement pool

--- tests/models/animals/test_carcasses_and_poo.py
+++ tests/models/animals/test_carcasses_and_poo.py
@@ -13,3 +13,14 @@ class TestCarcassPool:
 
         c1 = CarcassPool(1000.7, 1)
         assert c1.stored_energy == 1000.7
+
+
+class TestExcrementPool:
+    """Test the ExcrementPool class."""
+
+    def test_initialization(self):
+        """Testing initialization of CarcassPool."""
+        from virtual_rainforest.models.animals.carcasses_and_poo import ExcrementPool
+
+        poo = ExcrementPool(25.0)
+        assert poo.stored_energy == 25.0
