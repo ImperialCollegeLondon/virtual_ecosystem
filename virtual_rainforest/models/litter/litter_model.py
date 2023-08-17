ea28d67e72bdd0677e9b437663f6601e29316f52
Jacob Cook <jc2017@ic.ac.uk>
2023-08-17 13:41:49 +0100

    Forgot to update litter model as well

--- virtual_rainforest/models/litter/litter_model.py
+++ virtual_rainforest/models/litter/litter_model.py
@@ -24,10 +24,11 @@ import numpy as np
 from pint import Quantity
 
 from virtual_rainforest.core.base_model import BaseModel
+from virtual_rainforest.core.constants import load_constants
 from virtual_rainforest.core.data import Data
 from virtual_rainforest.core.exceptions import InitialisationError
 from virtual_rainforest.core.logger import LOGGER
-from virtual_rainforest.core.utils import load_constants, set_layer_roles
+from virtual_rainforest.core.utils import set_layer_roles
 from virtual_rainforest.models.litter.constants import LitterConsts
 from virtual_rainforest.models.litter.litter_pools import calculate_litter_pool_updates
 
