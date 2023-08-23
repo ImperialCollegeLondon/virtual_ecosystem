b1a0d77735855e358c9610fb42700506e34df166
Jacob Cook <jc2017@ic.ac.uk>
2023-08-23 14:04:15 +0100

    Simplified function to make section title

--- docs/source/_ext/autodoc_constants_registry.py
+++ docs/source/_ext/autodoc_constants_registry.py
@@ -1,7 +1,5 @@
 """Script defining a custom autodoc directive to document the constants registry."""
 
-from typing import Any
-
 from docutils import nodes
 from docutils.parsers.rst import Directive
 from sphinx.application import Sphinx
@@ -21,17 +19,17 @@ class AutoDocConstantsRegistry(Directive):
 
     def run(self) -> list[str]:
         """TODO - Work out what the docstring should be here."""
-        classes_dict = get_classes_from_registry()
         content = []
 
-        for class_name, class_obj in classes_dict.items():
-            class_rst = generate_constants_class_rst(class_name, class_obj)
-            content.extend(class_rst)
+        for model_name, classes_dict in CONSTANTS_REGISTRY.items():
+            for class_name in classes_dict.keys():
+                class_rst = generate_constants_class_rst(model_name, class_name)
+                content.extend(class_rst)
 
         return content
 
 
-def generate_constants_class_rst(class_name: str, class_obj: Any) -> list[str]:
+def generate_constants_class_rst(model_name: str, class_name: str) -> list[str]:
     """Generate rst appropriate to document each constants class.
 
     TODO - Add description of arguments, and returns
@@ -40,21 +38,7 @@ def generate_constants_class_rst(class_name: str, class_obj: Any) -> list[str]:
     rst_nodes = []
 
     # Add a title for the class
-    class_subtitle = nodes.subtitle(text=class_name)
-    rst_nodes.append(class_subtitle)
+    class_title = nodes.title(text=f"{model_name}: {class_name}")
+    rst_nodes.append(class_title)
 
     return rst_nodes
-
-
-def get_classes_from_registry() -> dict[str, Any]:
-    """TODO - Add a sensible docstring here."""
-
-    flattened_class = {}
-
-    for model_name in CONSTANTS_REGISTRY:
-        for class_name in CONSTANTS_REGISTRY[model_name]:
-            flattened_class.update(
-                {class_name: CONSTANTS_REGISTRY[model_name][class_name]}
-            )
-
-    return flattened_class
