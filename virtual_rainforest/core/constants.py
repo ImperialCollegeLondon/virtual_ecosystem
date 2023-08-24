96ef1220b1080730d5e2f1cb60dcec5e708f7574
Jacob Cook <jc2017@ic.ac.uk>
2023-08-24 11:40:37 +0100

    Added some mention of model constants to make things a bit clearer

--- virtual_rainforest/core/constants.py
+++ virtual_rainforest/core/constants.py
@@ -1,11 +1,11 @@
 """The :mod:`~virtual_rainforest.core.constants` module is used to store constants that
 are used across the Virtual Rainforest. This includes universal constants, such as the
-strength of gravity, but also constants that are shared between multiple models, such as
-the depth of the biogeochemically active soil layer.
+strength of gravity, but also model constants that are shared between multiple models,
+such as the depth of the biogeochemically active soil layer.
 
 At the moment, no constants are actually stored in this module. It currently only
-contains the :attr:`CONSTANTS_REGISTRY`, which all constants classes should be
-registered in across models. This allows for all constants to be documented neatly.
+contains the :attr:`CONSTANTS_REGISTRY`, which all model constants classes should be
+registered in. This allows for all model constants to be documented neatly.
 """  # noqa: D205, D415
 
 import dataclasses
@@ -16,7 +16,7 @@ from virtual_rainforest.core.exceptions import ConfigurationError
 from virtual_rainforest.core.logger import LOGGER
 
 CONSTANTS_REGISTRY: dict[str, dict[str, Callable]] = {}
-"""A registry for all the constants data classes.
+"""A registry for all the model constants data classes.
 
 :meta hide-value:
 """
