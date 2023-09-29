957aeff88d9a9e9749739f0590eba2e7df0c739c
David Orme <davidorme@users.noreply.github.com>
2023-09-29 22:58:15 +0100

    Stripped schema stuff from old home in config

--- virtual_rainforest/core/config.py
+++ virtual_rainforest/core/config.py
@@ -26,20 +26,19 @@ The base Virtual Rainforest module will automatically import modules when it is
 imported, which ensures that all modules schemas are registered in
 :attr:`SCHEMA_REGISTRY`.
 """  # noqa: D205, D415
-import json
 import sys
 from collections.abc import Sequence
 from copy import deepcopy
 from pathlib import Path
-from typing import Any, Iterator, Union
+from typing import Any, Union
 
-import dpath.util  # type: ignore
 import tomli_w
-from jsonschema import Draft202012Validator, FormatChecker, exceptions, validators
+from jsonschema import FormatChecker
 
 from virtual_rainforest.core.exceptions import ConfigurationError
 from virtual_rainforest.core.logger import LOGGER
 from virtual_rainforest.core.registry import MODULE_REGISTRY, register_module
+from virtual_rainforest.core.schema import ValidatorWithDefaults, merge_schemas
 
 if sys.version_info[:2] >= (3, 11):
     import tomllib
@@ -51,163 +50,6 @@ SCHEMA_REGISTRY: dict[str, Any] = {}
 
 :meta hide-value:
 """
-
-#  Set up a JSON Schema validator that fills in default values
-
-
-def set_defaults(
-    validator: type[Draft202012Validator],
-    properties: dict[str, Any],
-    instance: dict[str, Any],
-    schema: dict[str, Any],
-) -> Iterator:
-    """Generate an iterator to populate schema defaults.
-
-    This function is used to extend the Draft202012Validator to include automatic
-    insertion of default values from a schema where values are not specified. The
-    function signature follows the required JSON schema pattern:
-
-    https://python-jsonschema.readthedocs.io/en/latest/creating/
-
-    Args:
-        validator: a validator instance,
-        properties: the value of the property being validated within the instance
-        instance: the instance
-        schema: the schema
-
-    Returns:
-        An iterator to be applied to JSON schema entries.
-    """
-    for property, subschema in properties.items():
-        if "default" in subschema:
-            instance.setdefault(property, subschema["default"])
-
-    for error in Draft202012Validator.VALIDATORS["properties"](
-        validator,
-        properties,
-        instance,
-        schema,
-    ):
-        yield error
-
-
-ValidatorWithDefaults = validators.extend(
-    Draft202012Validator, {"properties": set_defaults}
-)
-
-# Schema handling functions
-
-
-def register_schema(module_name: str, schema_file_path: Path) -> None:
-    """Simple function to add configuration schema to the registry.
-
-    Args:
-        module_name: The name to register the schema under
-        schema_file_path: The file path to the JSON Schema file for the model
-
-    Raises:
-        ValueError: If the module name has already been used to register a schema
-    """
-
-    if module_name in SCHEMA_REGISTRY:
-        excep = ValueError(f"The module schema for {module_name} is already registered")
-        LOGGER.critical(excep)
-        raise excep
-
-    try:
-        SCHEMA_REGISTRY[module_name] = load_schema(module_name, schema_file_path)
-    except Exception as excep:
-        LOGGER.critical(f"Schema registration for {module_name} failed: check log")
-        raise excep
-
-    LOGGER.info("Schema registered for module %s: %s ", module_name, schema_file_path)
-
-
-def load_schema(module_name: str, schema_file_path: Path) -> dict:
-    """Function to load the JSON schema for a module.
-
-    This function tries to load a JSON schema file and then - if the JSON loaded
-    correctly - checks that the JSON provides a valid JSON Schema.
-
-    Args:
-        module_name: The name to register the schema under
-        schema_file_path: The file path to the JSON Schema file
-
-    Raises:
-        FileNotFoundError: the schema path does not exist
-        json.JSONDecodeError: the file at the schema path is not valid JSON
-        jsonschema.SchemaError: the file contents are not valid JSON Schema
-        ValueError: the JSON Schema is missing required keys
-    """
-
-    # Try and get the contents of the JSON schema file
-    try:
-        json_schema = json.load(open(schema_file_path))
-    except FileNotFoundError:
-        fnf_error = FileNotFoundError(f"Schema file not found {schema_file_path}.")
-        LOGGER.error(fnf_error)
-        raise fnf_error
-    except json.JSONDecodeError as excep:
-        LOGGER.error(f"JSON error in schema file {schema_file_path}")
-        raise excep
-
-    # Check that this is a valid schema - deliberately log a separate message and let
-    # the schema traceback output rather than replacing it.
-    try:
-        ValidatorWithDefaults.check_schema(json_schema)
-    except exceptions.SchemaError as excep:
-        LOGGER.error(f"Module schema invalid in: {schema_file_path}")
-        raise excep
-
-    # Check that relevant keys are included - deliberately re-raising as ValueError here
-    # as the KeyError has built in quoting and is expecting to provide simple missing
-    # key messages.
-    try:
-        json_schema["properties"][module_name]
-        json_schema["required"]
-    except KeyError as excep:
-        to_raise = ValueError(f"Missing key in module schema {module_name}: {excep}")
-        LOGGER.error(to_raise)
-        raise to_raise
-
-    return json_schema
-
-
-def merge_schemas(schemas: dict[str, dict[str, Any]]) -> dict[str, Any]:
-    """Merge the validation schemas for desired modules.
-
-    The method merges a set of schemas for a set of desired modules into a single
-    integrated schema that can then be used to validate a merged configuration for those
-    modules.
-
-    Args:
-        schema: A dictionary of schemas keyed by module name
-
-    Returns:
-        An integrated schema combining the modules.
-    """
-
-    # Construct combined schema for all relevant modules
-    comb_schema: dict = {"type": "object", "properties": {}, "required": []}
-
-    # Loop over expected modules amd add them to the combined schema
-    for this_module, this_schema in schemas.items():
-        comb_schema["properties"][this_module] = this_schema["properties"][this_module]
-        # Add module name to list of required modules
-        comb_schema["required"].append(this_module)
-
-    p_paths = []
-    # Recursively search for all instances of properties in the schema
-    for path, value in dpath.util.search(comb_schema, "**/properties", yielded=True):
-        # Remove final properties instance from path so that additionalProperties ends
-        # up in the right place
-        p_paths.append("" if path == "properties" else path[:-11])
-
-    # Set additional properties to false everywhere that properties are defined
-    for path in p_paths:
-        dpath.util.new(comb_schema, f"{path}/additionalProperties", False)
-
-    return comb_schema
 
 
 def config_merge(
