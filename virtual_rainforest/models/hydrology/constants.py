1d2fa8e9f066e34e57127ad42a5df3ee4e3dc14f
vgro <vivienne.groner@gmx.de>
2023-10-09 10:07:40 +0100

    updated description of constants

--- virtual_rainforest/models/hydrology/constants.py
+++ virtual_rainforest/models/hydrology/constants.py
@@ -16,7 +16,9 @@ class HydroConsts:
     """Dataclass to store all constants for the `hydrology` model."""
 
     soil_moisture_capacity: float = 0.9
-    """Soil moisture capacity, also known as field capacity or water holding capacity,
+    """Soil moisture capacity, unitless.
+
+    The soil moisture capacity, also known as field capacity or water holding capacity,
     refers to the maximum amount of water that a soil can retain against the force of
     gravity after it has been saturated and excess water has drained away. The value is
     soil type specific, the format here is volumentic relative water content (unitless,
@@ -24,7 +26,9 @@ class HydroConsts:
     """
 
     soil_moisture_residual: float = 0.1
-    """Residual soil moisture refers to the water that remains in the soil after
+    """Residual soil moisture, unitless.
+
+    The residual soil moisture refers to the water that remains in the soil after
     prolonged drainage due to the force of gravity. It is the water that is tightly held
     by soil particles and is not easily available for plant roots to extract. The value
     is soil specific, the format here is volumentic relative water content (unitless,
@@ -32,14 +36,18 @@ class HydroConsts:
     """
 
     hydraulic_conductivity: float = 0.001
-    """Hydraulic conductivity (m/s) is the measure of a soil's ability to transmit water
+    """Hydraulic conductivity, [m s-1].
+
+    The hydraulic conductivity is the measure of a soil's ability to transmit water
     through its pores. More specifically, is defined as the volumetric flow rate of
     water passing through a unit cross-sectional area of soil under a unit hydraulic
     gradient (pressure difference).
     """
 
     hydraulic_gradient: float = 0.01
-    """The hydraulic gradient (m/m) is a measure of the change in hydraulic head
+    """The hydraulic gradient, [m m-1].
+
+    The hydraulic gradient is a measure of the change in hydraulic head
     (pressure) per unit of distance in a particular direction within a fluid or porous
     medium, such as soil or an aquifer. It represents the driving force behind the
     movement of water and indicates the direction in which water will flow.
@@ -49,8 +57,10 @@ class HydroConsts:
     """Factor to convert variable unit from seconds to day."""
 
     nonlinearily_parameter: float = 2.0
-    """Nonlinearity parameter n (dimensionless) in Mualem-van Genuchten model for
-    hydraulic conductivity :cite:p:`van_genuchten_closed-form_1980`."""
+    """Nonlinearity parameter n (dimensionless) in Mualem-van Genuchten model.
+
+    This parameter is a fitting shape parameters of soil water retention curve, see
+    :cite:p:`van_genuchten_closed-form_1980`."""
 
     meters_to_mm: float = 1000
     """Factor to convert variable unit from meters to millimeters."""
@@ -59,49 +69,65 @@ class HydroConsts:
     """Factor to convert variable unit from Celsius to Kelvin."""
 
     density_air: float = 1.225
-    """Density of air under standard atmosphere, kg m-3"""
+    """Density of air under standard atmosphere, [kg m-3]"""
 
     latent_heat_vapourisation: float = 2.45
-    """Latent heat of vapourisation under standard atmosphere, MJ kg-1"""
+    """Latent heat of vapourisation under standard atmosphere, [MJ kg-1]"""
 
     gas_constant_water_vapour: float = 461.51
-    """Gas constant for water vapour, J kg-1 K-1"""
+    """Gas constant for water vapour, [J kg-1 K-1]"""
 
     heat_transfer_coefficient: float = 12.5
     """Heat transfer coefficient, :cite:p:`van_de_griend_bare_1994` """
 
     stream_flow_capacity: float = 5000.0
-    """Stream flow capacity, mm per timestep. This is curretly an arbitrary value, but
-    could be used in the future to flag flood events."""
+    """Stream flow capacity, [mm per timestep].
+
+    This parameter represents the maximum capacity of an average stream in the model.
+    At the moment, this is set as an arbitrary value, but could be used in the future to
+    flag flood events."""
 
     intercept_param_1: float = 0.935
-    """Parameter in equation that estimates maximum canopy interception capacity after
+    """Interception parameter 1.
+
+    Parameter in equation that estimates maximum canopy interception capacity after
     :cite:t:`von_hoyningen-huene_interzeption_1981`."""
 
     intercept_param_2: float = 0.498
-    """Parameter in equation that estimates maximum canopy interception capacity after
+    """Interception parameter 2.
+
+    Parameter in equation that estimates maximum canopy interception capacity after
     :cite:t:`von_hoyningen-huene_interzeption_1981`."""
 
     intercept_param_3: float = 0.00575
-    """Parameter in equation that estimates maximum canopy interception capacity after
+    """Interception parameter 3.
+
+    Parameter in equation that estimates maximum canopy interception capacity after
     :cite:t:`von_hoyningen-huene_interzeption_1981`."""
 
     veg_density_param: float = 0.046
-    """Parameter used to estimate vegetation density for maximum canopy interception
-    capacity estimate after :cite:t:`von_hoyningen-huene_interzeption_1981`."""
+    """Parameter to estimate vegetation density for maximum canopy interception.
+
+    This parameter is used to estimate the water holding capacity of a canopy after
+    :cite:t:`von_hoyningen-huene_interzeption_1981`."""
 
     groundwater_capacity: float = 500
-    """Ground water storage capacity in millimeters. This might be
-    replaced with the implementation of below ground horizontal flow."""
+    """Ground water storage capacity, [mm].
+
+    This parameter indicates how much water can be stored in the ground water reservoir
+    which affects the vertical flow of water and the horizontal sub-surface flow. This
+    parameter is currently set to an arbitrary value and might."""
 
     infiltration_shape_parameter: float = 1.0
-    """Empirical shape parameter that affects how much of the water available for
-    infiltration goes directly to groundwater via preferential bypass flow. A value of
+    """Empirical infiltration shape parameter, unitless.
+
+    This parameter affects how much of the water available for infiltration goes
+    directly to groundwater via preferential bypass flow. A value of
     0 means all surface water goes directly to groundwater, a value of 1 gives a linear
     relation between soil moisture and bypass flow."""
 
     air_entry_water_potential: float = -3.815
-    """Water potential at which soil pores begin to aerate [kPa].
+    """Water potential at which soil pores begin to aerate, [kPa].
 
     The constant is used to estimate soil water potential from soil moisture. As this
     estimation is a stopgap this constant probably shouldn't become a core constant. The
@@ -130,11 +156,14 @@ class HydroConsts:
     """
 
     max_percolation_rate_uzlz: float = 1
-    """Maximum perclation rate between pper and lower groundwater zone, [mm d-1]"""
+    """Maximum percolation rate between upper and lower groundwater zone, [mm d-1]"""
 
     groundwater_loss: float = 1
-    """Constant amount of water that never rejoins the river channel and is lost beyond
-    the catchment boundaries or to deep groundwater systems, [mm]"""
+    """Constant ground water loss, [mm].
+
+    This parameter defines the constant amount of water that never rejoins the river
+    channel and is lost beyond the catchment boundaries or to deep groundwater systems.
+    """
 
     reservoir_const_upper_groundwater: float = 20
     """Reservoir constant for the upper groundwater layer, [days]"""
