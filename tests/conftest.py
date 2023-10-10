02d8cedd00cd40b6d66c55788339adbcebb0d6be
David Orme <davidorme@users.noreply.github.com>
2023-10-10 11:06:11 +0100

    Added log_check functionality to check a subset of the caplog.records (last one for example)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 """Collection of fixtures to assist the testing scripts."""
 from logging import DEBUG
-from typing import Any
+from typing import Any, Optional
 
 import numpy as np
 import pytest
@@ -15,21 +15,33 @@ from virtual_rainforest.core.logger import LOGGER
 LOGGER.setLevel(DEBUG)
 
 
-def log_check(caplog: pytest.LogCaptureFixture, expected_log: tuple[tuple]) -> None:
+def log_check(
+    caplog: pytest.LogCaptureFixture,
+    expected_log: tuple[tuple],
+    subset: Optional[slice] = None,
+) -> None:
     """Helper function to check that the captured log is as expected.
 
     Arguments:
         caplog: An instance of the caplog fixture
         expected_log: An iterable of 2-tuples containing the log level and message.
+        subset: Only check a specified subset of the captured log.
     """
 
-    assert len(expected_log) == len(caplog.records)
+    # caplog.records is just a list of LogRecord objects, so can use a slice to drop
+    # down to a subset of the records.
+    if subset is None:
+        captured_records = caplog.records
+    else:
+        captured_records = caplog.records[subset]
+
+    assert len(expected_log) == len(captured_records)
 
     assert all(
-        [exp[0] == rec.levelno for exp, rec in zip(expected_log, caplog.records)]
+        [exp[0] == rec.levelno for exp, rec in zip(expected_log, captured_records)]
     )
     assert all(
-        [exp[1] in rec.message for exp, rec in zip(expected_log, caplog.records)]
+        [exp[1] in rec.message for exp, rec in zip(expected_log, captured_records)]
     )
 
 
