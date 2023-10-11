a1bc13becf6b10914771de34d860d99c4b8252f1
David Orme <davidorme@users.noreply.github.com>
2023-10-11 10:20:35 +0100

    Extended vr_run test to tidy up file handler on test failure

--- tests/core/test_cli_integration.py
+++ tests/core/test_cli_integration.py
@@ -1,6 +1,9 @@
 """An integration test for the VR command-line interface."""
+
 from pathlib import Path
 from tempfile import TemporaryDirectory
+
+import pytest
 
 
 def test_vr_run_install_example(capsys):
@@ -17,25 +20,34 @@ def test_vr_run_install_example(capsys):
 
 def test_vr_run(capsys):
     """Test that the CLI can successfully run with example data."""
+    # import virtual_rainforest.core  # noqa #F401
+    from virtual_rainforest.core.logger import remove_file_logger
     from virtual_rainforest.entry_points import vr_run_cli
 
     with TemporaryDirectory() as tempdir:
-        # Install the example directory to run it - tested above - and consume the
-        # resulting stdout
-        vr_run_cli(args_list=["--install-example", tempdir])
-        _ = capsys.readouterr()
+        try:
+            # Install the example directory to run it - tested above - and consume the
+            # resulting stdout
+            vr_run_cli(args_list=["--install-example", tempdir])
+            _ = capsys.readouterr()
 
-        example_dir = Path(tempdir) / "vr_example"
-        vr_run_cli(
-            args_list=[
-                str(example_dir),
-                "--outpath",
-                str(example_dir),
-                "--logfile",
-                str(example_dir / "vr_example.log"),
-            ]
-        )
+            example_dir = Path(tempdir) / "vr_example"
+            vr_run_cli(
+                args_list=[
+                    str(example_dir),
+                    "--outpath",
+                    str(example_dir),
+                    "--logfile",
+                    str(example_dir / "vr_example.log"),
+                ]
+            )
 
-        captured = capsys.readouterr()
-        expected = "VR run complete."
-        assert captured.out.startswith(expected)
+            captured = capsys.readouterr()
+            expected = "VR run complete."
+            assert captured.out.startswith(expected)
+        except Exception as excep:
+            # If the code above fails then tidy up the logger to restore normal stream
+            # logging rather than leaving all other tests logging to the file and then
+            # fail the test.
+            remove_file_logger()
+            pytest.fail(msg=str(excep))
