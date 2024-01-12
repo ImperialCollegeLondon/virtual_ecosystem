6b4d8365824a61f664f7feb454c936590afc9f66
vgro <vivienne.groner@gmx.de>
2024-01-12 16:32:04 +0000

    initial absorbed radiation test updated

--- tests/models/abiotic/test_energy_balance.py
+++ tests/models/abiotic/test_energy_balance.py
@@ -1,20 +1,39 @@
 """Test module for abiotic.abiotic_model.energy_balance.py."""
 
 import numpy as np
+from xarray import DataArray
 
 
-def test_initialise_absorbed_radiation():
+def test_initialise_absorbed_radiation(dummy_climate_data):
     """Test initial absorbed radiation has correct dimensions."""
 
     from virtual_rainforest.models.abiotic.energy_balance import (
         initialise_absorbed_radiation,
     )
 
+    d = dummy_climate_data
+    leaf_area_index_true = d["leaf_area_index"][
+        d["leaf_area_index"]["layer_roles"] == "canopy"
+    ].dropna(dim="layers", how="all")
+    layer_heights_canopy = d["layer_heights"][
+        d["leaf_area_index"]["layer_roles"] == "canopy"
+    ].dropna(dim="layers", how="all")
+    topofcanopy_radiation = DataArray(np.array([100.0, 100.0, 100.0]), dims="cell_id")
+
     result = initialise_absorbed_radiation(
-        topofcanopy_radiation=np.array([100, 50]),
-        leaf_area_index=np.array([[1, 1, 1], [1, 1, 1]]),
-        canopy_layer_heights=np.array([[10, 5, 2], [10, 5, 2]]),
+        topofcanopy_radiation=topofcanopy_radiation.to_numpy(),
+        leaf_area_index=leaf_area_index_true.to_numpy(),
+        layer_heights=layer_heights_canopy.T.to_numpy(),  # TODO check why .T needed
         light_extinction_coefficient=0.01,
     )
 
-    np.testing.assert_allclose(result, np.array([[5, 3, 2], [3, 2, 1]]))
+    np.testing.assert_allclose(
+        result,
+        np.array(
+            [
+                [9.516258, 8.610666, 7.791253],
+                [9.516258, 8.610666, 7.791253],
+                [9.516258, 8.610666, 7.791253],
+            ]
+        ),
+    )
