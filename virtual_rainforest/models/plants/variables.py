d68bd7cfa829d8742cfbf5f4599815f96ed949c4
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2024-02-15 15:54:01 +0000

    Add example of adding a variable

---
+++ virtual_rainforest/models/plants/variables.py
@@ -0,0 +1,11 @@
+"""Variables associated to the plans modudle."""
+
+import virtual_rainforest.core.variables as variables
+
+variables.Variable(
+    name="plant_cohorts_cell_id",
+    description="The cell ID of the cohort.",
+    units="",
+    var_type="int",
+    axis=(),
+)
