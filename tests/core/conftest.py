0ab35b7b30386de04d14b6c2fb213485e46557ae
David Orme <davidorme@users.noreply.github.com>
2024-06-17 11:12:54 +0100

    Moving axis validator fixtures

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -1,5 +1,7 @@
 """Fixtures for use in core testing."""
 
+from typing import Any
+
 import pytest
 from xarray import DataArray
 
@@ -58,3 +60,36 @@ def fixture_data(fixture_square_grid_simple):
     data["existing_var"] = DataArray([1, 2, 3, 4], dims=("cell_id",))
 
     return data
+
+
+@pytest.fixture
+def new_axis_validators():
+    """Create new axis validators to test methods and registration."""
+    from virtual_ecosystem.core.axes import AxisValidator
+    from virtual_ecosystem.core.grid import Grid
+
+    # Create a new subclass.
+    class TestAxis(AxisValidator):
+        core_axis = "testing"
+        dim_names = frozenset(["test"])
+
+        def can_validate(self, value: DataArray, grid: Grid, **kwargs: Any) -> bool:
+            return True if value.sum() > 10 else False
+
+        def run_validation(
+            self, value: DataArray, grid: Grid, **kwargs: Any
+        ) -> DataArray:
+            return value * 2
+
+    # Create a new duplicate subclass to check mutual exclusivity test
+    class TestAxis2(AxisValidator):
+        core_axis = "testing"
+        dim_names = frozenset(["test"])
+
+        def can_validate(self, value: DataArray, grid: Grid, **kwargs: Any) -> bool:
+            return True if value.sum() > 10 else False
+
+        def run_validation(
+            self, value: DataArray, grid: Grid, **kwargs: Any
+        ) -> DataArray:
+            return value * 2
