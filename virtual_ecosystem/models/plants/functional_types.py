2d9e2658812fca86ff10ab00db9f588ceccf15c5
David Orme <davidorme@users.noreply.github.com>
2025-01-07 16:26:43 +0000

    Adding new pyrealm flora loader and testing

--- virtual_ecosystem/models/plants/functional_types.py
+++ virtual_ecosystem/models/plants/functional_types.py
@@ -7,6 +7,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from pyrealm.demography.flora import Flora as pyrealmFlora
+
 from virtual_ecosystem.core.config import Config, ConfigurationError
 from virtual_ecosystem.core.logger import LOGGER
 
@@ -15,9 +17,9 @@ from virtual_ecosystem.core.logger import LOGGER
 class PlantFunctionalType:
     """Data class containing plant functional type definitions."""
 
-    pft_name: str
+    name: str
     """The name of the plant functional type."""
-    max_height: float
+    h_max: float
     """The maximum stem height of the plant functional type."""
 
 
@@ -35,13 +37,13 @@ class Flora(dict):
     Args:
         pfts: A list of ``PlantFunctionalType`` instances, which must not have
             duplicated
-            :attr:`~virtual_ecosystem.models.plants.functional_types.PlantFunctionalType.pft_name`
+            :attr:`~virtual_ecosystem.models.plants.functional_types.PlantFunctionalType.name`
             attributes.
     """
 
     def __init__(self, pfts: list[PlantFunctionalType]) -> None:
         # Get the names and check there are no duplicates
-        pft_names = [p.pft_name for p in pfts]
+        pft_names = [p.name for p in pfts]
         if len(pft_names) != len(set(pft_names)):
             msg = "Duplicated plant functional type names in creating Flora instance."
             LOGGER.critical(msg)
@@ -71,11 +73,11 @@ class Flora(dict):
             for ftype in config["plants"]["ftypes"]:
                 try:
                     pft = PlantFunctionalType(**ftype)
-                    if pft.pft_name in pft_dict:
-                        msg = f"Config duplicates plant functional type {pft.pft_name}."
+                    if pft.name in pft_dict:
+                        msg = f"Config duplicates plant functional type {pft.name}."
                         LOGGER.critical(msg)
                         raise ConfigurationError(msg)
-                    pft_dict[pft.pft_name] = pft
+                    pft_dict[pft.name] = pft
                 except Exception as excep:
                     LOGGER.critical(
                         f"Error generating plant functional type: {excep!s}"
@@ -87,3 +89,38 @@ class Flora(dict):
             raise ConfigurationError(msg)
 
         return cls(list(pft_dict.values()))
+
+
+def get_flora_from_config(config: Config) -> pyrealmFlora:
+    """Generate a Flora object from a Virtual Ecosystem configuration.
+
+    Args:
+        config: A validated Virtual Ecosystem model configuration object.
+
+    Returns:
+        A populated :class:`pyrealm.demography.flora.Flora` instance
+    """
+
+    if "plants" not in config:
+        msg = "Model configuration for plants model not found."
+        LOGGER.critical(msg)
+        raise ConfigurationError(msg)
+
+    # Check for duplicate definition options - this should be prevented by the schema
+    # definition setting oneOf the following two is required
+    if (
+        "pft_definition" in config["plants"]
+        and "pft_definitions_path" in config["plants"]
+    ):
+        msg = "Do not use both `pft_definitions_path` and `pft_definition` in config."
+        LOGGER.critical(msg)
+        raise ConfigurationError(msg)
+
+    # If the data is provided in the configuration, load that
+    if "pft_definition" in config["plants"]:
+        # TODO: currently need to rename this property to match internal expectation in
+        # pyrealm, change here if this is fixed/aligned.
+        pft_data = {"pft": config["plants"]["pft_definition"]}
+        return pyrealmFlora._from_file_data(pft_data)
+
+    return pyrealmFlora.from_csv(config["plants"]["pft_definitions_path"])
