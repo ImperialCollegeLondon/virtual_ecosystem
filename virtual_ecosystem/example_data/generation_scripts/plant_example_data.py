0df2ebaea6f79ffe145216295caa3f56848a2612
David Orme <davidorme@users.noreply.github.com>
2025-01-30 20:49:39 +0000

    Moving plant data to plant config, adding plant cohort CSV generation

--- virtual_ecosystem/example_data/generation_scripts/plant_example_data.py
+++ virtual_ecosystem/example_data/generation_scripts/plant_example_data.py
@@ -48,3 +48,10 @@ data["photosynthetic_photon_flux_density"] = DataArray(
 data["time"] = DataArray(time, coords={"time_index": time_index})
 
 data.to_netcdf("../data/example_plant_data.nc")
+
+# Write cohort data to CSV file as an alternative form of this data source
+df = data.drop_vars(
+    ["photosynthetic_photon_flux_density", "time", "time_index", "cell_id"]
+).to_pandas()
+
+df.to_csv("../data/example_plant_cohorts.nc", index=False)
