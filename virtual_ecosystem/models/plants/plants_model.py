764b74ec65f2f6138fa2b8fd680890db12b1e10f
Jacob Cook <jc2017@ic.ac.uk>
2025-02-12 15:51:06 +0000

    Added safeguard so that plants don't uptake a negative amount of nutrients if nutrient concentrations are negative

--- virtual_ecosystem/models/plants/plants_model.py
+++ virtual_ecosystem/models/plants/plants_model.py
@@ -543,7 +543,20 @@ class PlantsModel(
             up function, and does not link to plant dynamics in any way.
         """
 
-        # Assume plants can take 0.1% of the available nutrient per day
-        self.data["plant_ammonium_uptake"] = self.data["soil_n_pool_ammonium"] * 0.001
-        self.data["plant_nitrate_uptake"] = self.data["soil_n_pool_nitrate"] * 0.001
-        self.data["plant_phosphorus_uptake"] = self.data["soil_p_pool_labile"] * 0.001
+        # Assume plants can take 0.1% of the available nutrient per day, uptake stops
+        # (rather than goes negative) if the soil nutrient concentrations go negative
+        self.data["plant_ammonium_uptake"] = xr.where(
+            self.data["soil_n_pool_ammonium"] > 0.0,
+            self.data["soil_n_pool_ammonium"] * 0.001,
+            0.0,
+        )
+        self.data["plant_nitrate_uptake"] = xr.where(
+            self.data["soil_n_pool_nitrate"] > 0.0,
+            self.data["soil_n_pool_nitrate"] * 0.001,
+            0.0,
+        )
+        self.data["plant_phosphorus_uptake"] = xr.where(
+            self.data["soil_p_pool_labile"] > 0.0,
+            self.data["soil_p_pool_labile"] * 0.001,
+            0.0,
+        )
