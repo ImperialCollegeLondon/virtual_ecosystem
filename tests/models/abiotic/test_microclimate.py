421ca73f9d91dbaee85f65c1159b30915659bf78
vgro <vivienne.groner@gmx.de>
2025-03-05 11:51:14 +0000

    removed unused tests

--- tests/models/abiotic/test_microclimate.py
+++ tests/models/abiotic/test_microclimate.py
@@ -27,55 +27,6 @@ def test_run_microclimate(dummy_climate_data, fixture_core_components):
         core_constants=CoreConsts(),
     )
 
-    # exp_longwave_emission = lyr_str.from_template()
-    # exp_longwave_emission[lyr_str.index_flux_layers] = np.array(
-    #     [424.66886, 420.013465, 412.137266, 391.685362]
-    # )[:, None]
-    # np.testing.assert_allclose(
-    #     result["longwave_emission"][lyr_str.index_flux_layers],
-    #     exp_longwave_emission[lyr_str.index_flux_layers],
-    #     rtol=1e-04,
-    #     atol=1e-04,
-    # )
-
-    # exp_sens_heat = lyr_str.from_template()
-    # exp_sens_heat[lyr_str.index_flux_layers] = np.array(
-    #     [-67.539934, -64.089671, -58.191538, -20.43684]
-    # )[:, None]
-    # np.testing.assert_allclose(
-    #     result["sensible_heat_flux"][lyr_str.index_flux_layers],
-    #     exp_sens_heat[lyr_str.index_flux_layers],
-    #     rtol=1e-04,
-    #     atol=1e-04,
-    # )
-
-    # exp_lat_heat = lyr_str.from_template()
-    # exp_lat_heat[lyr_str.index_flux_layers] = np.array(
-    #     [
-    #         [66.835395, 66.835395, 66.835395, 66.835395],
-    #         [66.835395, 66.835395, 66.835395, 66.835395],
-    #         [66.835395, 66.835395, 66.835395, 66.835395],
-    #         [0.003342, 0.033418, 0.334177, 0.334177],
-    #     ]
-    # )
-    # np.testing.assert_allclose(
-    #     result["latent_heat_flux"],
-    #     exp_lat_heat,
-    #     rtol=1e-04,
-    #     atol=1e-04,
-    # )
-
-    # exp_wind = lyr_str.from_template()
-    # exp_wind[lyr_str.index_filled_atmosphere] = np.array(
-    #     [0.727122, 0.615474, 0.001, 0.001, 0.001]
-    # )[:, None]
-    # np.testing.assert_allclose(
-    #     result["wind_speed"],
-    #     exp_wind,
-    #     rtol=1e-04,
-    #     atol=1e-04,
-    # )
-
     exp_soiltemp = lyr_str.from_template()
     exp_soiltemp[lyr_str.index_all_soil] = np.array([17.357065, 19.959777])[:, None]
     np.testing.assert_allclose(
