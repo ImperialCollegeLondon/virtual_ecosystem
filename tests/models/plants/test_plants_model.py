8d8bc1612c68a24ccd48f5ca5c201207f83aed03
David Orme <davidorme@users.noreply.github.com>
2025-07-04 11:16:26 +0100

    Stop recruitment and mortality in update test

--- tests/models/plants/test_plants_model.py
+++ tests/models/plants/test_plants_model.py
@@ -343,6 +343,9 @@ def test_PlantsModel_update(fxt_plants_model, fixture_canopy_layer_data):
 
     wipe_canopy_layers(fxt_plants_model)
 
+    fxt_plants_model.per_update_interval_propagule_recruitment_probability = 0
+    fxt_plants_model.per_update_interval_stem_mortality_probability = 0
+
     # Check reset
     fxt_plants_model.update(time_index=0)
 
