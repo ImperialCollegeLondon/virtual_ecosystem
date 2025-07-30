fixed calculation for layer thickness for varying canopy and temperature update

tests/models/abiotic/test_abiotic_model.py
@@ -387,11 +387,11 @@ def test_setup_abiotic_model(
 
     exp_air_temp[lyr_strct.index_filled_atmosphere] = np.array(
         [
-            [30.000122, 30.000067, np.nan, np.nan],
-            [29.913604, 29.954362, np.nan, np.nan],
-            [29.408153, np.nan, np.nan, np.nan],
-            [28.545607, np.nan, np.nan, np.nan],
-            [22.478502, 24.294355, 25.796236, 25.796236],
+            [30.000105, 30.000091, 30.000047, 30.000047],
+            [29.925696, 29.938505, 29.970583, 29.970583],
+            [29.42155, 29.613321, np.nan, np.nan],
+            [28.558176, np.nan, np.nan, np.nan],
+            [23.158518, 26.130326, 29.416104, 29.416104],
         ]
     )
     xr.testing.assert_allclose(model.data["air_temperature"], exp_air_temp)

tests/models/abiotic/test_energy_balance.py
@@ -4,6 +4,9 @@ import numpy as np
 import pytest
 
 from virtual_ecosystem.core.constants import CoreConsts
+from virtual_ecosystem.models.abiotic.abiotic_tools import (
+    compute_layer_thickness_for_varying_canopy,
+)
 from virtual_ecosystem.models.abiotic.constants import AbioticConsts
 
 
@@ -316,7 +319,7 @@ def test_solve_canopy_temperature(
 def test_update_air_temperature(
     dummy_climate_data_varying_canopy, fixture_core_components
 ):
-    """Test update air and canopy temperatures."""
+    """Test update air temperature in canopy."""
     from virtual_ecosystem.models.abiotic.energy_balance import (
         update_air_temperature,
     )
@@ -325,9 +328,9 @@ def test_update_air_temperature(
     lystr = fixture_core_components.layer_structure
     canopy_index = lystr.index_filled_canopy
 
-    heights = data["layer_heights"][lystr.index_filled_atmosphere].to_numpy()
-    heights_with_base = np.vstack([heights, np.zeros(heights.shape[1])])
-    above_ground_layer_thickness = -np.diff(heights_with_base, axis=0)
+    above_ground_layer_thickness = compute_layer_thickness_for_varying_canopy(
+        heights=data["layer_heights"][lystr.index_filled_atmosphere].to_numpy()
+    )
 
     updated_air_temperature = update_air_temperature(
         air_temperature=data["air_temperature"][canopy_index].to_numpy(),
@@ -342,9 +345,9 @@ def test_update_air_temperature(
 
     exp_result = np.array(
         [
-            [29.806235, 29.806235, np.nan, np.nan],
-            [28.840201, np.nan, np.nan, np.nan],
-            [27.188754, np.nan, np.nan, np.nan],
+            [29.883755, 29.883755, 29.857958, 29.857958],
+            [28.902139, 28.886732, np.nan, np.nan],
+            [27.224235, np.nan, np.nan, np.nan],
         ]
     )
     np.testing.assert_allclose(updated_air_temperature, exp_result, rtol=1e-4)
@@ -361,12 +364,12 @@ def test_update_humidity_vpd(
 
     data = dummy_climate_data_varying_canopy
     lystr = fixture_core_components.layer_structure
-    atm_index = lystr.index_filled_atmosphere
     canopy_index = lystr.index_filled_canopy
+    atm_index = lystr.index_filled_atmosphere
 
-    heights = data["layer_heights"][atm_index].to_numpy()
-    heights_with_base = np.vstack([heights, np.zeros(heights.shape[1])])
-    above_ground_layer_thickness = -np.diff(heights_with_base, axis=0)
+    above_ground_layer_thickness = compute_layer_thickness_for_varying_canopy(
+        heights=data["layer_heights"][atm_index].to_numpy()
+    )
 
     evapotranspiration = data["transpiration"] + data["canopy_evaporation"]
     saturated_vapour_pressure = np.array(

tests/models/abiotic/test_microclimate.py
@@ -65,37 +65,32 @@ def test_run_microclimate(dummy_climate_data_varying_canopy, fixture_core_compon
     )
 
     # Check that all air temperature values fall within a reasonable expected range
-    # mask = np.isnan(
-    #     dummy_climate_data_varying_canopy["air_temperature"][
-    #         lyr_str.index_filled_atmosphere
-    #     ]
-    # )
-    # air_temp_result = result["air_temperature"][lyr_str.index_filled_atmosphere]
-    # assert np.all((air_temp_result[~mask] >= 16.0) & (air_temp_result[~mask] <= 36.0))
+    air_temp_result = result["air_temperature"].isel(
+        layers=lyr_str.index_filled_atmosphere
+    )
 
-    print(result["air_temperature"])
-    # np.array([[30.00023402, 30.00024546,         nan,         nan],
-    #    [29.83391357, 29.83371163,         nan,         nan],
-    #    [28.88179021,         nan,         nan,         nan],
-    #    [27.21446276,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [21.10594219, 21.07185157, 20.73094533, 20.73094533],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    # ]
-    # )
+    mask = ~np.isnan(
+        dummy_climate_data_varying_canopy["air_temperature"].isel(
+            layers=lyr_str.index_filled_atmosphere
+        )
+    )
+
+    # Use the mask as a DataArray for .where()
+    valid_values = air_temp_result.where(mask)
+
+    # Now drop the NaNs (i.e., masked values)
+    valid_values_clean = valid_values.dropna(
+        dim="layers", how="any"
+    )  # or "all" if you're doing 2D
+
+    # Now do the test
+    assert ((valid_values_clean >= 16.0) & (valid_values_clean <= 36.0)).all()
 
     exp_relhum = lyr_str.from_template()
     exp_relhum[lyr_str.index_filled_atmosphere] = np.array(
         [
-            [100, 100, np.nan, np.nan],
-            [100, 100, np.nan, np.nan],
+            [100, 100, 100, 100],
+            [100, 100, 100, 100],
             [100, np.nan, np.nan, np.nan],
             [100, np.nan, np.nan, np.nan],
             [100, 100, 100, 100],
@@ -131,25 +126,29 @@ def test_run_microclimate_subdaily(
         core_constants=CoreConsts(),
         pyrealm_const=PyrealmConst(),
     )
-    print(result["air_temperature"])
-    # np.array([[29.98536857, 29.99051257,         nan,         nan],
-    #    [33.7172163 , 31.35657893,         nan,         nan],
-    #    [34.28084653,         nan,         nan,         nan],
-    #    [32.64513234,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [21.87141268, 21.73480416, 20.36795001, 20.36795001],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan]])
 
     # Check that all air temperature values fall within a reasonable expected range
-    air_temp_result = result["air_temperature"][lyr_str.index_filled_atmosphere]
-    assert np.all((air_temp_result >= 16.0) & (air_temp_result <= 36.0))
+    # Check that all air temperature values fall within a reasonable expected range
+    air_temp_result = result["air_temperature"].isel(
+        layers=lyr_str.index_filled_atmosphere
+    )
+
+    mask = ~np.isnan(
+        dummy_climate_data_varying_canopy["air_temperature"].isel(
+            layers=lyr_str.index_filled_atmosphere
+        )
+    )
+
+    # Use the mask as a DataArray for .where()
+    valid_values = air_temp_result.where(mask)
+
+    # Now drop the NaNs (i.e., masked values)
+    valid_values_clean = valid_values.dropna(
+        dim="layers", how="any"
+    )  # or "all" if you're doing 2D
+
+    # Now do the test
+    assert ((valid_values_clean >= 16.0) & (valid_values_clean <= 36.0)).all()
 
 
 def test_run_microclimate_minutes(
@@ -172,21 +171,26 @@ def test_run_microclimate_minutes(
         core_constants=CoreConsts(),
         pyrealm_const=PyrealmConst(),
     )
-    print(result["air_temperature"])
-    # np.array([[30.00164585, 30.00056018,         nan,         nan],
-    #    [28.8318953 , 29.62049735,         nan,         nan],
-    #    [21.30954266,         nan,         nan,         nan],
-    #    [19.59780849,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan],
-    #    [21.10594219, 21.07185157, 20.73094533, 20.73094533],
-    #    [        nan,         nan,         nan,         nan],
-    #    [        nan,         nan,         nan,         nan]])
+
     # Check that all air temperature values fall within a reasonable expected range
-    air_temp_result = result["air_temperature"][lyr_str.index_filled_atmosphere]
-    assert np.all((air_temp_result >= 16.0) & (air_temp_result <= 36.0))
+    # Check that all air temperature values fall within a reasonable expected range
+    air_temp_result = result["air_temperature"].isel(
+        layers=lyr_str.index_filled_atmosphere
+    )
+
+    mask = ~np.isnan(
+        dummy_climate_data_varying_canopy["air_temperature"].isel(
+            layers=lyr_str.index_filled_atmosphere
+        )
+    )
+
+    # Use the mask as a DataArray for .where()
+    valid_values = air_temp_result.where(mask)
+
+    # Now drop the NaNs (i.e., masked values)
+    valid_values_clean = valid_values.dropna(
+        dim="layers", how="any"
+    )  # or "all" if you're doing 2D
+
+    # Now do the test
+    assert ((valid_values_clean >= 16.0) & (valid_values_clean <= 36.0)).all()