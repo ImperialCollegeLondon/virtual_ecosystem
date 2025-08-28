8e8fce4fef5742344605dc2ba3a383ac059ea861
Taran Rallings <tralling@ic.ac.uk>
2025-08-28 13:55:13 +0100

    Added pom and bacteria to DietType.

--- virtual_ecosystem/models/animal/animal_traits.py
+++ virtual_ecosystem/models/animal/animal_traits.py
@@ -14,7 +14,11 @@ class MetabolicType(Enum):
 
 
 class DietType(Flag):
-    """Enumeration for diet resource types."""
+    """Enumeration for diet resource types.
+
+    TODO: refine categorizations
+
+    """
 
     ALGAE = auto()
     DETRITUS = auto()
@@ -32,6 +36,8 @@ class DietType(Flag):
     WASTE = auto()
     WOOD = auto()
     NONFEEDING = auto()
+    POM = auto()
+    BACTERIA = auto()
 
     HERBIVORE = (
         ALGAE
@@ -40,6 +46,8 @@ class DietType(Flag):
         | FOLIAGE
         | FRUIT
         | FUNGUS
+        | POM  # temp
+        | BACTERIA  # temp
         | SEEDS
         | NECTAR
         | WOOD
