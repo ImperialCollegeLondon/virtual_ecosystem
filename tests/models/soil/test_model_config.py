c374ac77d16390f5238bfc6aeb3799941b0df887
David Orme <davidorme@users.noreply.github.com>
2025-10-23 22:02:23 +0100

    Updating soil tests, swapping to more constants fixtures

--- tests/models/soil/test_model_config.py
+++ tests/models/soil/test_model_config.py
@@ -6,6 +6,8 @@ from itertools import permutations
 import pytest
 from pydantic import ValidationError
 
+from virtual_ecosystem.core.exceptions import ConfigurationError
+
 
 def test_SoilConstants_pH():
     """Test the custom validation on SoilConstants model.
@@ -108,7 +110,12 @@ def test_SoilConfiguration_microbial_group_definition(data, outcome):
         pytest.param(
             {"taxonomic_group": "bacteria", "reproductive_allocation": 0.3},
             pytest.raises(ValidationError),
-            id="user provided bad",
+            id="user provided fruiting bacteria",
+        ),
+        pytest.param(
+            {"taxonomic_group": "archaea", "reproductive_allocation": 0.3},
+            pytest.raises(ValidationError),
+            id="user provided archaea",
         ),
     ),
 )
@@ -118,3 +125,137 @@ def test_SoilMicrobialGroup(data, outcome):
 
     with outcome:
         _ = SoilMicrobialGroup(**data)
+
+
+@pytest.mark.parametrize(
+    argnames=["cfg_strings"],
+    argvalues=[
+        pytest.param(
+            """
+            [[soil.enzyme_class_definition]]
+            source = "bacteria"
+            substrate = "pom"
+            [[soil.enzyme_class_definition]]
+            source = "bacteria"
+            substrate = "maom"
+            [[soil.enzyme_class_definition]]
+            source = "fungi"
+            substrate = "pom"
+            """,
+            id="missing_fungi_maom",
+        ),
+        pytest.param(  # archaea included but they shouldn't be
+            """
+            [[soil.enzyme_class_definition]]
+            source = "bacteria"
+            substrate = "pom"
+            [[soil.enzyme_class_definition]]
+            source = "bacteria"
+            substrate = "maom"
+            [[soil.enzyme_class_definition]]
+            source = "fungi"
+            substrate = "pom"
+            [[soil.enzyme_class_definition]]
+            source = "fungi"
+            substrate = "maom"
+            [[soil.enzyme_class_definition]]
+            source = "fungi"
+            substrate = "phosphate"
+            """,
+            id="unexpected_phosphatase",
+        ),
+        pytest.param(
+            """
+            [[soil.enzyme_class_definition]]
+            source = "bacteria"
+            substrate = "pom"
+
+            [[soil.enzyme_class_definition]]
+            source = "fungi"
+            substrate = "phosphate"
+            """,
+            id="missing_most_enzymes_and_unexpected_phosphatase",
+        ),
+    ],
+)
+def test_soil_enzyme_definition_via_toml(caplog, cfg_strings):
+    """Check that bad configs generate errors during enzyme class generation.
+
+    This is a cut down version of testing of the old make_full_set_of_enzymes_errors
+    function, retained as a bit of a belt and braces test for testing the full path from
+    TOML.
+    """
+
+    from virtual_ecosystem.core.config_builder import (
+        ConfigurationLoader,
+        generate_configuration,
+    )
+
+    config_data = ConfigurationLoader(cfg_strings=cfg_strings)
+    caplog.clear()
+
+    with pytest.raises(ConfigurationError):
+        _ = generate_configuration(config_data.data)
+
+
+@pytest.mark.parametrize(
+    argnames="cfg_strings",
+    argvalues=[
+        pytest.param(
+            """
+            [[soil.microbial_group_definition]]
+            name = "bacteria"
+            taxonomic_group = "bacteria"
+            """,
+            id="missing_fungi",
+        ),
+        pytest.param(
+            """
+            [[soil.microbial_group_definition]]
+            name = "bacteria"
+            taxonomic_group = "bacteria"
+            [[soil.microbial_group_definition]]
+            name = "saprotrophic_fungi"
+            taxonomic_group = "fungi"
+            [[soil.microbial_group_definition]]
+            name = "arbuscular_mycorrhiza"
+            taxonomic_group = "fungi"
+            [[soil.microbial_group_definition]]
+            name = "ectomycorrhiza"
+            taxonomic_group = "fungi"
+            [[soil.microbial_group_definition]]
+            name = "archaea"
+            taxonomic_group = "archaea"
+            """,
+            id="unexpected_archaea",
+        ),
+        pytest.param(
+            """
+            [[soil.microbial_group_definition]]
+            name = "bacteria"
+            taxonomic_group = "bacteria"
+            [[soil.microbial_group_definition]]
+            name = "archaea"
+            taxonomic_group = "archaea"
+
+            """,
+            id="missing_fungi_and_unexpected_archaea",
+        ),
+    ],
+)
+def test_make_full_set_of_microbial_groups_errors(cfg_strings):
+    """Check that bad configs generate errors during microbial group generation.
+
+    This is a cut down version of testing of the old
+    test_make_full_set_of_microbial_groups_errors function, retained as a bit of a belt
+    and braces test for testing the full path from TOML.
+    """
+    from virtual_ecosystem.core.config_builder import (
+        ConfigurationLoader,
+        generate_configuration,
+    )
+
+    config_data = ConfigurationLoader(cfg_strings=cfg_strings)
+
+    with pytest.raises(ConfigurationError):
+        _ = generate_configuration(config_data.data)
