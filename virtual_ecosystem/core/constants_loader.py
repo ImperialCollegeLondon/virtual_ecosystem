008d2e9536eaf7909203436472ab8eca994f01b3
David Orme <davidorme@users.noreply.github.com>
2025-11-03 10:16:38 +0800

    Shutting down constants_loader functionality - uses retired registry attributes

--- virtual_ecosystem/core/constants_loader.py
+++ virtual_ecosystem/core/constants_loader.py
@@ -9,7 +9,6 @@ configuration details for that constants dataclass from a
 from typing import Any
 
 from virtual_ecosystem.core.config import Config
-from virtual_ecosystem.core.exceptions import ConfigurationError
 from virtual_ecosystem.core.logger import LOGGER
 from virtual_ecosystem.core.registry import MODULE_REGISTRY
 
@@ -35,33 +34,37 @@ def load_constants(config: Config, module_name: str, class_name: str) -> Any:
         LOGGER.critical(msg)
         raise KeyError(msg)
 
-    if class_name not in MODULE_REGISTRY[module_name].constants_classes:
-        msg = f"Unknown constants class: {module_name}.{class_name}"
-        LOGGER.critical(msg)
-        raise KeyError(msg)
+    # if class_name not in MODULE_REGISTRY[module_name].constants_classes:
+    #     msg = f"Unknown constants class: {module_name}.{class_name}"
+    #     LOGGER.critical(msg)
+    #     raise KeyError(msg)
 
-    # Catch attempts to generate a constants class from a registered module that is not
-    # included in the configuration object
-    if module_name not in config:
-        msg = f"Configuration does not include module: {module_name}"
-        LOGGER.critical(msg)
-        raise KeyError(msg)
+    # # Catch attempts to generate a constants class from a registered module that
+    # # is not
+    # # included in the configuration object
+    # if module_name not in config:
+    #     msg = f"Configuration does not include module: {module_name}"
+    #     LOGGER.critical(msg)
+    #     raise KeyError(msg)
 
-    # Get the class and extract the constant class configuration if present
-    constants_class = MODULE_REGISTRY[module_name].constants_classes[class_name]
-    constants_config = (
-        config[module_name]["constants"][class_name]
-        if "constants" in config[module_name]
-        and class_name in config[module_name]["constants"]
-        else {}
-    )
+    # # Get the class and extract the constant class configuration if present
+    # constants_class = MODULE_REGISTRY[module_name].constants_classes[class_name]
+    # constants_config = (
+    #     config[module_name]["constants"][class_name]
+    #     if "constants" in config[module_name]
+    #     and class_name in config[module_name]["constants"]
+    #     else {}
+    # )
 
-    # Try and configure the constants class
-    try:
-        class_instance = constants_class.from_config(constants_config)
-    except ConfigurationError:
-        LOGGER.critical(f"Could not initialise {module_name}.{class_name} from config")
-        raise
+    # # Try and configure the constants class
+    # try:
+    #     class_instance = constants_class.from_config(constants_config)
+    # except ConfigurationError:
+    #     LOGGER.critical(
+    #       f"Could not initialise {module_name}.{class_name} from config"
+    #     )
+    #     raise
 
-    LOGGER.info(f"Initialised {module_name}.{class_name} from config")
-    return class_instance
+    # LOGGER.info(f"Initialised {module_name}.{class_name} from config")
+
+    return None
