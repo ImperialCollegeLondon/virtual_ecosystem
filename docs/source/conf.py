e93d13001b0476813960b1bb9b52d69eaab9ea8c
David Orme <davidorme@users.noreply.github.com>
2026-01-09 18:56:15 +0000

    Apparently getting an extra ethical ads DIV from somewhere

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,7 +268,7 @@ exclude_patterns = [
 
 html_theme = "pydata_sphinx_theme"
 
-html_sidebars = {"**": ["sidebar_funder", "sidebar-nav-bs", "sidebar-ethical-ads"]}
+html_sidebars = {"**": ["sidebar_funder", "sidebar-nav-bs"]}
 
 html_theme_options = {
     "logo": {
