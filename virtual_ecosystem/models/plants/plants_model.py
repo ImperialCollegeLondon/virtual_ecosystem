46817a55732547ee03a36fe267be13f012a80245
jacobcook1995 <jc2017@ic.ac.uk>
2026-01-09 11:22:37 +0000

    Removed unused plant reproductive tissue variables from the plants model

--- virtual_ecosystem/models/plants/plants_model.py
+++ virtual_ecosystem/models/plants/plants_model.py
@@ -92,10 +92,10 @@ class PlantsModel(
         "fallen_fruit_cnp",
         "fallen_seeds_per_fruit",
         "fallen_seeds_cnp",
-        "fallen_non_propagule_c_mass",  # NOTE - will be deprecated in #1131
-        "plant_rt_turnover_n_mass",  # NOTE - will be deprecated in #1131
-        "plant_rt_turnover_p_mass",  # NOTE - will be deprecated in #1131
-        "plant_reproductive_tissue_turnover",  # NOTE - will be deprecated in #1131
+        "fallen_non_propagule_c_mass",  # NOTE - will be deprecated in #1132
+        "plant_rt_turnover_n_mass",  # NOTE - will be deprecated in #1132
+        "plant_rt_turnover_p_mass",  # NOTE - will be deprecated in #1132
+        "plant_reproductive_tissue_turnover",  # NOTE - will be deprecated in #1132
         "subcanopy_seedbank_litter_cnp",
         "subcanopy_vegetation_litter_cnp",
         "subcanopy_vegetation_cnp",
@@ -111,7 +111,7 @@ class PlantsModel(
         "plant_p_uptake_arbuscular",
         "plant_p_uptake_ecto",
         "plant_phosphorus_uptake",
-        "plant_reproductive_tissue_lignin",
+        "plant_reproductive_tissue_lignin",  # NOTE - will be deprecated in #1132
         "plant_symbiote_carbon_supply",
         "root_carbohydrate_exudation",
         "root_lignin",
@@ -153,8 +153,6 @@ class PlantsModel(
         "plant_phosphorus_uptake",
         "plant_reproductive_tissue_turnover",
         "plant_reproductive_tissue_lignin",
-        "plant_reproductive_tissue_turnover_c_n_ratio",
-        "plant_reproductive_tissue_turnover_c_p_ratio",
         "plant_rt_turnover_n_mass",
         "plant_rt_turnover_p_mass",
         "plant_symbiote_carbon_supply",
@@ -587,6 +585,7 @@ class PlantsModel(
 
         # The deprecated variables have been moved to a separate method to keep this one
         # current, but still need to be initialised until they are deleted in #1131:
+        # I can delete this as I will stop the litter model using the variables
         self.old_stoichiometry_ratios_to_depricate()
 
         # Initialize variables that hold one value per cell
@@ -677,11 +676,6 @@ class PlantsModel(
             swd=self.canopy_top_radiation,
             data_object_template=self.data_object_templates["cnp"],
         )
-
-        # Use new data variables to update the old ones for cross compatibility with
-        # litter and soil. Delete this after litter/soil are updated to use the new
-        # variable structure.
-        self.old_outputs_to_depricate()
 
         # Run the community data exporter
         self.exporter.dump(
@@ -1445,6 +1439,7 @@ class PlantsModel(
 
         return n_propagules, non_propagule_mass
 
+    # NOTE - will be deprecated in #1132
     def old_stoichiometry_ratios_to_depricate(self) -> None:
         """Temporary function to initialise variables for the litter and soil models.
 
@@ -1454,8 +1449,6 @@ class PlantsModel(
 
         vars_to_update = [
             "plant_reproductive_tissue_turnover",
-            "plant_reproductive_tissue_turnover_c_n_ratio",
-            "plant_reproductive_tissue_turnover_c_p_ratio",
             "plant_rt_turnover_n_mass",
             "plant_rt_turnover_p_mass",
             "fallen_non_propagule_c_mass",
@@ -1515,26 +1508,3 @@ class PlantsModel(
         time_interval_in_days = self.model_timing.update_interval_seconds / 86400
 
         return input_mass / (1000.0 * time_interval_in_days * self.grid.cell_area)
-
-    def old_outputs_to_depricate(self):
-        """Ensure all vars are here and correct."""
-
-        self.data["plant_reproductive_tissue_turnover"] = self.convert_to_litter_units(
-            input_mass=self.data["plant_reproductive_tissue_turnover"]
-        )
-
-        for element in ["N", "P"]:
-            # Update carbon to nutrient ratios for turnover pools
-
-            self.data[
-                f"plant_reproductive_tissue_turnover_c_{element.lower()}_ratio"
-            ] = np.divide(
-                self.data["plant_reproductive_tissue_turnover"],
-                self.data[f"plant_rt_turnover_{element.lower()}_mass"],
-                out=np.full_like(
-                    self.data["plant_reproductive_tissue_turnover"],
-                    np.inf,
-                    dtype=float,
-                ),
-                where=self.data[f"plant_rt_turnover_{element.lower()}_mass"] != 0,
-            )
