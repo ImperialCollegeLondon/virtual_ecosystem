06edc232b526c8a87b0ce1bd0da4294f91d4c52e
vgro <vivienne.groner@gmx.de>
2026-03-17 18:13:34 +0000

    try attributes in class docstring

--- virtual_ecosystem/models/hydrology/model_config.py
+++ virtual_ecosystem/models/hydrology/model_config.py
@@ -7,8 +7,6 @@ Note that soil parameters vary strongly with soil type and can change over time.
 current default values are average best estimates within reasonable bounds.
 """  # noqa: D205
 
-from typing import Annotated
-
 from pydantic import Field
 
 from virtual_ecosystem.core.configuration import Configuration, ModelConfigurationRoot
@@ -169,19 +167,25 @@ class HydrologyConstants(Configuration):
 
 
 class HydrologyConfiguration(ModelConfigurationRoot):
-    """Root configuration class for the hydrology model."""
+    """Root configuration class for the hydrology model.
+
+    Attributes:
+        initial_soil_moisture: Initial soil moisture for all layers
+    initial_groundwater_saturation: Initial ground water saturation for all layers
+        unitless
+    constants: Constants values for hydrology model.
+    p_wet_wet: Probability a wet day follows a wet day.
+    p_wet_dry: Probability a wet day follows a dry day.
+    rainfall_shape_parameter: Shape parameter of the Gamma distribution controlling
+        rainfall variability.
+    rainfall_scale_parameter: Scale parameter of the Gamma distribution controlling
+        magnitude of rainfall.
+    """
 
     initial_soil_moisture: float = Field(ge=0, le=1, default=0.5)
-    """Initial soil moisture for all layers"""
     initial_groundwater_saturation: float = Field(ge=0, le=1, default=0.9)
-    """Initial ground water saturation for all layers, unitless"""
     constants: HydrologyConstants = HydrologyConstants()
-    """Constants values for hydrology model."""
-    p_wet_wet: Annotated[float, Field(ge=0.3, le=0.8)] = 0.6
-    """Probability a wet day follows a wet day."""
-    p_wet_dry: Annotated[float, Field(ge=0.1, le=0.5)] = 0.3
-    """Probability a wet day follows a dry day."""
-    rainfall_shape_parameter: Annotated[float, Field(ge=0.7, le=4.0)] = 1.5
-    """Shape parameter of the Gamma distribution controlling rainfall variability."""
-    rainfall_scale_parameter: Annotated[float, Field(ge=0.1, le=2.0)] = 1.0
-    """Scale parameter of the Gamma distribution controlling magnitude of rainfall."""
+    p_wet_wet: float = Field(ge=0.3, le=0.8, default=0.6)
+    p_wet_dry: float = Field(ge=0.1, le=0.5, default=0.3)
+    rainfall_shape_parameter: float = Field(ge=0.7, le=4.0, default=1.5)
+    rainfall_scale_parameter: float = Field(ge=0.1, le=2.0, default=1.0)
