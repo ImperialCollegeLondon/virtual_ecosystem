2cf57b26a5584f2a5dd067c52507f58668f3214a
kirkImperial <o.kirk@ic.ac.uk>
2026-04-20 16:57:36 +0100

    merge

--- profiling/main.py
+++ profiling/main.py
@@ -64,13 +64,8 @@ output_name = f"VirEco__py{ver}__truncated_at_step_{truncation}"
 time_stamp = (datetime.datetime.now()).strftime("%Y-%m-%d_at_%H-%M")
 
 command_options = {
-<<<<<<< HEAD
     "windows": f".\\.venv\\Python{ver}\\Scripts\\python.exe -m cProfile -o {profiler_folder}/{output_name}.prof profiling/run.py --ver={ver} --path={path} --truncate={truncation}",
     "linux": f"./.venv/Python{ver}/bin/python -m cProfile -o {profiler_folder}/{output_name}.prof profiling/run.py --ver={ver} --path={path} --truncate={truncation}",
-=======
-    "windows": f".\\ve{ver}\\Scripts\\python.exe -m cProfile -o {profiler_folder}/{output_name}.prof profiling/run.py --ver={ver} --path={path} --truncate={truncation}",
-    "linux": f"./ve{ver}/bin/python -m cProfile -o {profiler_folder}/{output_name}.prof profiling/run.py --ver={ver} --path={path} --truncate={truncation}",
->>>>>>> 6af072624c405525f12e6836d39e5a7d7680b065
 }
 command_options["mac"] = command_options["linux"]
 
