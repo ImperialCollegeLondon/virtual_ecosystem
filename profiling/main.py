cfe9d9e06f344817ba7fbe03ca9c2eecb5933456
kirkImperial <o.kirk@ic.ac.uk>
2026-04-20 13:56:19 +0100

    feat: profiling code

---
+++ profiling/main.py
@@ -0,0 +1,81 @@
+"""Running the Virtual Ecosystem via the `ve_run_cli()` with a specified Python version, then saving the profiling outs from `cProfile`.
+
+Call this script in the terminal with `python profiling/main.py` after installing the specified Python version has a virtual environment directory installed.
+See the general Python docs for further information on virtual environments and installation: https://docs.python.org/dev/tutorial/venv.html#tut-venv"""
+
+import datetime, os, subprocess, shutil
+
+
+# Designate the path from theroot directory.
+path = "profiling"
+
+# Check example data exists to start the simulation.
+if not os.path.exists(f"{path}/config"):
+    raise FileNotFoundError(
+        f"Config folder not found at {path}/config. Please ensure the path variable is correctly set and the config folder exists."
+    )
+
+# Create a folder for the cProfile outputs.
+profiler_folder = f"{path}/cProfile_outputs"
+if not os.path.exists(profiler_folder):
+    os.makedirs(profiler_folder)
+
+# Create a folder for the VE outputs.
+out_folder = f"{path}/out"
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
+
+# Clear the current ./out folder.
+for filename in os.listdir(out_folder):
+    file_path = os.path.join(out_folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+
+# Set custom variables.
+user_os = "windows"  # options: "windows", "linux", "mac".
+"""Python version, can be written as "3.14" or "3_14"."""
+ver = "3.14"
+ver = ver.replace(".", "_")
+
+"""How many steps to run, can be an integer or `None`."""
+truncation = 0
+
+
+# Generate terminal command to run `ve_run_cli()` via cProfile.
+output_name = f"VE__python{ver}__truncated_at_step_{truncation}"
+
+# Note: the `time_stamp` variable cannot contain colons (:) in Windows as these are not valid characters for that OS.
+time_stamp = (datetime.datetime.now()).strftime("%Y-%m-%d_at_%H-%M")
+
+command_options = {
+    "windows": f".\\ve{ver}\\Scripts\\python.exe -m cProfile -o {profiler_folder}/{output_name}.prof {path}/run.py --ver={ver} --path={path} --truncate={truncation}",
+    "linux": f"./ve{ver}/bin/python -m cProfile -o {profiler_folder}/{output_name}.prof {path}/run.py --ver={ver} --path={path} --truncate={truncation}",
+    "mac": f"./ve{ver}/bin/python -m cProfile -o {profiler_folder}/{output_name}.prof {path}/run.py --ver={ver} --path={path} --truncate={truncation}",
+}
+command = command_options.get(user_os, None)
+if command is None:
+    raise ValueError(
+        "Invalid user_os value. Please choose from 'windows', 'linux', or 'mac'."
+    )
+
+# Run the terminal command within this script via the `subprocess` library.
+subprocess.run(command.split(), shell=True)
+
+
+# The terminal command if you want to view the results table and/or visual breakdown.
+print(f"python -m snakeviz {profiler_folder}/{output_name}.prof")
+
+# Copy over the output to a nested outputs folder sorted by Python version types for historic runs.
+nested_profiler_folder = f"{profiler_folder}/python{ver}"
+if not os.path.exists(nested_profiler_folder):
+    os.makedirs(nested_profiler_folder)
+shutil.copy(
+    f"{profiler_folder}/{output_name}.prof",
+    f"{nested_profiler_folder}/{time_stamp}__{output_name}.prof",
+)
