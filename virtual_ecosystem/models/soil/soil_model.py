5eeb7c0becc0fb93188c82fa282060f92df403d0
Jacob Cook <32497818+jacobcook1995@users.noreply.github.com>
2026-04-21 14:47:22 +0100

    Update virtual_ecosystem/models/soil/soil_model.py
    Co-authored-by: David Orme <davidorme@users.noreply.github.com>

--- virtual_ecosystem/models/soil/soil_model.py
+++ virtual_ecosystem/models/soil/soil_model.py
@@ -578,7 +578,7 @@ class SoilModel(
         else:
             subset = self.data[var]
 
-        return bool((subset.isnull() | np.isinf(subset)).any())
+        return bool( ~ np.all(np.isfinite(subset)))
 
     def convert_fruiting_body_production_to_rate(
         self, total_production: DataArray
