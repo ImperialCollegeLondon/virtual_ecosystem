50f876654560f517e07d2ea9127fb7a5101eda4a
vgro <vivienne.groner@gmx.de>
2026-07-01 16:45:04 +0100

    adjust nighttime aero resistance soil

--- virtual_ecosystem/models/abiotic/model_config.py
+++ virtual_ecosystem/models/abiotic/model_config.py
@@ -142,7 +142,7 @@ class AbioticConstants(AbioticSharedConstants):
     aerodynamic_resistance_canopy_night: float = 50.0
     """Aerodynamic resistance of the canopy at night, [s m-1]."""
 
-    aerodynamic_resistance_soil_night: float = 50.0
+    aerodynamic_resistance_soil_night: float = 100.0
     """Aerodynamic resistance of the soil at night, [s m-1]."""
 
     aerodynamic_resistance_canopy_day: float = 20.0
