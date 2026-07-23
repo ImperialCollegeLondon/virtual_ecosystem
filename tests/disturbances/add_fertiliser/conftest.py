277a3e59f76e7c077a1ec9ddec25b000d983ef13
jacobcook1995 <jc2017@ic.ac.uk>
2026-07-23 10:30:15 +0100

    Wrote basic example fertiliser disturbance

---
+++ tests/disturbances/add_fertiliser/conftest.py
@@ -0,0 +1,57 @@
+"""Fixtures for add-Fertiliser tests."""
+
+import pytest
+
+
+@pytest.fixture
+def fixture_fertiliser_init_data(fixture_core_components):
+    """Creates a dummy data object for use in fertiliser addition tests."""
+    from xarray import DataArray
+
+    from virtual_ecosystem.core.data import Data
+
+    # Setup the data object with four cells.
+    data = Data(fixture_core_components.grid)
+
+    data_values = {
+        "soil_n_pool_ammonium": [6.9619638e-5, 0.0049914624, 0.000229067, 0.0051955339],
+        "soil_n_pool_nitrate": [0.0024219014, 0.0044442996, 0.0003428348, 0.0131405173],
+    }
+
+    for var_name, var_values in data_values.items():
+        data[var_name] = DataArray(var_values, dims=["cell_id"])
+
+    return data
+
+
+@pytest.fixture
+def fixture_add_fertiliser_model(fixture_fertiliser_init_data):
+    """Create an add_fertiliser disturbance fixture based on the dummy data."""
+
+    from virtual_ecosystem.core.config_builder import (
+        ConfigurationLoader,
+        generate_configuration,
+    )
+    from virtual_ecosystem.core.core_components import CoreComponents
+    from virtual_ecosystem.disturbances.add_fertiliser.add_fertiliser_model import (
+        AddFertiliserModel,
+    )
+    from virtual_ecosystem.models.soil.soil_model import SoilModel
+
+    # Build the config object
+    cfg_strings = (
+        "[core]\n[core.grid]\ncell_nx = 2\ncell_ny = 2\n"
+        "[disturbance.add_fertiliser.constants]\n"
+        "nitrate_fraction=0.75\ninorganic_nitrogen_addition=2.5e-3"
+    )
+
+    config_data = ConfigurationLoader(cfg_strings=cfg_strings)
+    configuration = generate_configuration(config_data.data)
+    core_components = CoreComponents(configuration.core)
+
+    return AddFertiliserModel.from_config(
+        data=fixture_fertiliser_init_data,
+        configuration=configuration,
+        core_components=core_components,
+        models={"soil": SoilModel},
+    )
---
+++ virtual_ecosystem/disturbances/add_fertiliser/add_fertiliser_model.py
@@ -0,0 +1,137 @@
+"""A disturbance that captures the addition of fertiliser to the soil model.
+
+At present, this disturbance only affects the inorganic nitrogen pools (nitrate and
+ammonium). You can select the time steps for which fertiliser is applied, however, you
+cannot select specific grid cells to apply it for, i.e. it is always applied across the
+entire grid.
+
+For simplicity, the units used for fertiliser application are currently densities per
+(simulation) timestep, these almost certainly need to be altered to better match the
+units used in real world management.
+
+This disturbance is intended to be expanded over time so the limitations already
+mentioned can be removed (if it becomes necessary).
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+from virtual_ecosystem.core.base_model import BaseDisturbance, BaseModel
+from virtual_ecosystem.core.configuration import CompiledConfiguration
+from virtual_ecosystem.core.core_components import CoreComponents, DisturbanceTiming
+from virtual_ecosystem.core.data import Data
+from virtual_ecosystem.core.logger import LOGGER
+from virtual_ecosystem.disturbances.add_fertiliser.model_config import (
+    AddFertiliserConfiguration,
+    AddFertiliserConstants,
+)
+
+
+class AddFertiliserModel(
+    BaseDisturbance,
+    model_name="add_fertiliser",
+    disturbed_models=("soil",),
+    data_variables_disturbed=tuple(("soil_n_pool_ammonium", "soil_n_pool_nitrate")),
+):
+    """A disturbance that simulates fertilisation by adding nutrients to the soil.
+
+    Args:
+        data: The data object to be used in the model.
+        core_components: The core components used across models.
+        static: Boolean flag indicating if the model should run in static mode.
+    """
+
+    def __init__(
+        self,
+        data: Data,
+        models: dict[str, BaseModel],
+        disturbance_timing: DisturbanceTiming,
+        constants: AddFertiliserConstants = AddFertiliserConstants(),
+    ):
+        """Add_fertiliser init function.
+
+        The init function is used only to define class attributes.
+        """
+
+        super().__init__(
+            data=data, models=models, disturbance_timing=disturbance_timing
+        )
+
+        self.constants = constants
+        """Set of constants for the add_fertiliser disturbance."""
+
+    @classmethod
+    def from_config(
+        cls,
+        data: Data,
+        configuration: CompiledConfiguration,
+        core_components: CoreComponents,
+        models: dict[str, BaseModel],
+    ) -> AddFertiliserModel:
+        """Factory function to initialise fertiliser addition disturbance from config.
+
+        This function unpacks the relevant information from the configuration file, and
+        then uses it to initialise the model. If any information from the config is
+        invalid rather than returning an initialised model instance an error is raised.
+
+        Args:
+            data: A :class:`~virtual_ecosystem.core.data.Data` instance.
+            configuration: A validated Virtual Ecosystem model configuration object.
+            core_components: The core components used across models.
+            config: A validated Virtual Ecosystem model configuration object.
+            models: dictionary of :class:`~virtual_ecosystem.core.base_model.BaseModel`
+                instances of the models available in the simulation.
+        """
+
+        # Extract the validated model configuration from the complete compiled
+        # configuration. This syntax is odd but required to support static typing
+        model_configuration: AddFertiliserConfiguration = (
+            configuration.get_subconfiguration(
+                cls.model_name, AddFertiliserConfiguration
+            )
+        )
+
+        # Get the disturbance timing
+        model_timing = core_components.model_timing
+        disturbance_timing = DisturbanceTiming(
+            model_timing,
+            run_at=model_configuration.run_at,
+            run_every=model_configuration.run_every,
+        )
+
+        LOGGER.info(
+            "Information required to initialise the fertiliser addition disturbance "
+            "successfully extracted."
+        )
+
+        # Create the instance
+        inst = cls(
+            data=data,
+            models=models,
+            disturbance_timing=disturbance_timing,
+            constants=model_configuration.constants,
+        )
+
+        LOGGER.info(
+            "Fertiliser addition disturbance instance generated from configuration."
+        )
+        return inst
+
+    def _disturb(self, time_index: int, **kwargs: Any) -> None:
+        """Disturb the soil model with fertiliser addition.
+
+        This adds inorganic nitrogen to the nitrate and ammonium soil pools
+        """
+
+        # Calculate total addition to each pool
+        nitrate_addition = (
+            self.constants.inorganic_nitrogen_addition * self.constants.nitrate_fraction
+        )
+        ammonium_addition = self.constants.inorganic_nitrogen_addition * (
+            1 - self.constants.nitrate_fraction
+        )
+
+        # Add the fertiliser to the pools
+        self.data["soil_n_pool_nitrate"] += nitrate_addition
+        self.data["soil_n_pool_ammonium"] += ammonium_addition
