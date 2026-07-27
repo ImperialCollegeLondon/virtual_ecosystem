22b361d7eb19496b318d92c25671fcdcf109bda8
David Orme <davidorme@users.noreply.github.com>
2026-07-27 10:45:48 +0100

    Docstrings on VEFlora traits.

--- virtual_ecosystem/models/plants/functional_types.py
+++ virtual_ecosystem/models/plants/functional_types.py
@@ -27,25 +27,43 @@ class VEFlora(Flora):
     #      as tuples throughout. See:
     #           https://github.com/ImperialCollegeLondon/pyrealm/issues/695
 
-    # TODO - docstring these
-
     p_foliage_for_reproductive_tissue: tuple[float, ...] = (0.05,)
+    r"""The carbon allocation to reproductive tissue as a proportion of foliage mass (kg
+    kg-1)."""
     resp_rt: tuple[float, ...] = (0.05,)
+    r"""The annual respiration rate of reproductive tissue (:math:`r_{rt}`, kg kg-1)."""
     tau_rt: tuple[float, ...] = (1.0,)
+    r"""The annual turnover rate of reproductive tissue (:math:`\tau_{rt}`, kg kg-1)."""
     gpp_topslice: tuple[float, ...] = (0.1,)
+    r"""Carbon allocation to symbiotes as a proportion of NPP."""
     deadwood_c_n_ratio: tuple[float, ...] = (60.7,)
+    r"""Carbon/Nitrogen ratio of stem tissue."""
     deadwood_c_p_ratio: tuple[float, ...] = (856.5,)
+    r"""Carbon/Phosphorous ratio of stem tissue."""
     leaf_turnover_c_n_ratio: tuple[float, ...] = (25.5,)
+    r"""Carbon/Nitrogen ratio of senesced leaf tissue for turnover, after nutrient
+    reabsorption."""
     leaf_turnover_c_p_ratio: tuple[float, ...] = (415.0,)
+    r"""Carbon/Phosphorous ratio of senesced leaf tissue for turnover, after nutrient
+    reabsorption.."""
     plant_reproductive_tissue_turnover_c_n_ratio: tuple[float, ...] = (12.5,)
+    r"""Carbon/Nitrogen ratio of reproductive tissue."""
     plant_reproductive_tissue_turnover_c_p_ratio: tuple[float, ...] = (125.5,)
+    r"""Carbon/Phosphorous ratio of reproductive tissue."""
     root_turnover_c_n_ratio: tuple[float, ...] = (656.7,)
+    r"""Carbon/Nitrogen ratio of fine root tissue."""
     root_turnover_c_p_ratio: tuple[float, ...] = (45.6,)
+    r"""Carbon/Phosphorous ratio of fine root tissue."""
     foliage_c_n_ratio: tuple[float, ...] = (15.0,)
+    r"""Carbon/Nitrogen ratio of active leaf tissue."""
     foliage_c_p_ratio: tuple[float, ...] = (300.0,)
+    r"""Carbon/Phosphorous ratio of active leaf tissue."""
     c_mass_fruit_flesh: tuple[float, ...] = (5.0,)
+    r"""Carbon mass of total fruit flesh in reproductive structures."""
     c_mass_per_fruit_seed: tuple[float, ...] = (1.0,)
+    r"""Carbon mass of individual seeds in reproductive structures."""
     seeds_per_fruit: tuple[int, ...] = (2,)
+    r"""Number of seeds in each reproductive structure."""
 
     # Additional traits populated during validation - these hold the reference values
     # for lai and tau_f, which are modified by herbivory.
@@ -55,7 +73,9 @@ class VEFlora(Flora):
     #      now, but it probably makes more sense to add these directly after to Cohorts
     #      after running create_cohorts. Keep this for now.
     lai_base: None | tuple[float, ...] = None
+    r"""Reference variable holding the base LAI for the PFT."""
     tau_f_base: None | tuple[float, ...] = None
+    r"""Reference variable holding the base foliage turnover rate for the PFT."""
 
     @model_validator(mode="before")
     @classmethod
@@ -72,10 +92,10 @@ class VEFlora(Flora):
     @computed_field  # type: ignore[prop-decorator]
     @property
     def fruit_flesh_fraction(self) -> tuple[float, ...]:
-        """Calculate the fruit flesh fraction from the fruit traits.
-
-        The Flora properties are lists not arrays, so calculated by iteration.
-        """
+        """The proportion of fleshy tissue in reproductive structures, calculated
+        automatically from  fruit flesh fraction from the fruit traits.
+        """  # noqa: D205
+        # The Flora properties are lists not arrays, so calculated by iteration.
 
         return tuple(
             [
